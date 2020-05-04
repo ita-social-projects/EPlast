6d237ccb3da461398996b128245b6b9ac3e0814c
VolodymyrVerm <volodymyr33929@gmail.com>
2020-05-05 01:08:07 +0300

    Fix-problems-with-edit-user-photo

--- EPlast/EPlast/wwwroot/uncompiled/ts/UserProfile.js
+++ EPlast/EPlast/wwwroot/uncompiled/ts/UserProfile.js
@@ -98,14 +98,18 @@ $(document).ready(function () {
             }
         });
     });
-    $('#Upload-photo').click(function (e) {
+    $("#upload-file").change(function (e) {
         let input = document.getElementById("upload-file");
         var files = input.files;
         if (files[0] != undefined && files[0].size >= 3145728) {
             alert("Фото не може займати більше ніж 3 Мб");
+            $("#upload-file").val("");
             e.preventDefault();
             e.stopPropagation();
             return;
+        }
+        else {
+            $("#image").attr("src", URL.createObjectURL(input.files[0])).show();
         }
     });
 });
