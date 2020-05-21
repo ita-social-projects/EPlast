1003a7b43a6b928019f08112e3e4175c9c3a68b1
AndriiShainoha <55914660+AndriiShainoha@users.noreply.github.com>
2020-05-21 23:17:34 +0300

    Added changes in startup for AutoMapper and created mapping profiles

---
+++ EPlast/EPlast/Models/Mapping/ChangePasswordProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EPlast.BussinessLayer.DTO.Account;
+using EPlast.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPlast.Models.Mapping
+{
+    public class ChangePasswordProfile : Profile
+    {
+        public ChangePasswordProfile()
+        {
+            CreateMap<ChangePasswordDto, ChangePasswordViewModel>();
+            CreateMap<ChangePasswordViewModel, ChangePasswordDto>();
+        }
+    }
+}
