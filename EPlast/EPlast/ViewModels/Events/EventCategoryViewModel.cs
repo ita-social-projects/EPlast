a84faaf1bd140317c8ac48661fbf8f0eab3b7879
Roman <brych.126@gmail.com>
2020-05-25 02:58:54 +0300

    BR: Finish with refacoring

--- EPlast/EPlast/ViewModels/Events/EventCategoryViewModel.cs
+++ EPlast/EPlast/ViewModels/Events/EventCategoryViewModel.cs
@@ -1,10 +1,4 @@
-﻿using EPlast.DataAccess.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace EPlast.ViewModels.Events
+﻿namespace EPlast.ViewModels.Events
 {
     public class EventCategoryViewModel
     {
--- EPlast/EPlast/ViewModels/Events/EventUserViewModel.cs
+++ EPlast/EPlast/ViewModels/Events/EventUserViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using EPlast.DataAccess.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 
 namespace EPlast.ViewModels.Events
