ea109f75ef8a0226a468c9543c776385dda12d02
Michael Kunynets <xfreed1@gmail.com>
2020-05-26 13:13:34 +0300

    Change decesion to decision in IPDF interface

--- EPlast/EPlast.BussinessLayer/Interfaces/PDF/IPDFService.cs
+++ EPlast/EPlast.BussinessLayer/Interfaces/PDF/IPDFService.cs
@@ -5,7 +5,7 @@ namespace EPlast.BussinessLayer
 {
     public interface IPDFService
     {
-        Task<byte[]> DecesionCreatePDFAsync(Decesion pdfData);
+        Task<byte[]> DecisionCreatePDFAsync(int DecisionId);
 
         Task<byte[]> BlankCreatePDFAsync(BlankModel pdfData);
     }
