695945485bc3833dcf5bc1b8ebdb8b5f4b1b3633
ABashko <bashko.zllv@gmail.com>
2020-05-27 17:23:10 +0300

    Recreate CityAccess service

---
+++ EPlast/EPlast.BussinessLayer/Services/Interfaces/ICityAccessService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DatabaseEntities = EPlast.DataAccess.Entities;
+
+namespace EPlast.BussinessLayer.Services.Interfaces
+{
+    public interface ICityAccessService
+    {
+        Task<IEnumerable<DatabaseEntities.City>> GetCities(ClaimsPrincipal claimsPrincipal);
+        Task<bool> HasAccess(ClaimsPrincipal claimsPrincipal, int cityId);
+    }
+}
