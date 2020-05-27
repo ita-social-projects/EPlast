88c378b4e68721ba0b86b160786935ec43582fa3
ABashko <bashko.zllv@gmail.com>
2020-05-27 17:36:17 +0300

    Create DTOs for annual report

---
+++ EPlast/EPlast.BussinessLayer/DTO/CityManagementDTO.cs
@@ -0,0 +1,23 @@
+﻿using EPlast.BussinessLayer.DTO.City;
+
+namespace EPlast.BussinessLayer.DTO
+{
+    public class CityManagementDTO
+    {
+        public int ID { get; set; }
+
+        public CityLegalStatusType CityLegalStatusNew { get; set; }
+
+        public int? CityLegalStatusOldId { get; set; }
+        public CityLegalStatusDTO CityLegalStatusOld { get; set; }
+
+        public string UserId { get; set; }
+        public UserDTO CityAdminNew { get; set; }
+
+        public int? CityAdminOldId { get; set; }
+        public CityAdministrationDTO CityAdminOld { get; set; }
+
+        public int AnnualReportId { get; set; }
+        public AnnualReportDTO AnnualReport { get; set; }
+    }
+}
