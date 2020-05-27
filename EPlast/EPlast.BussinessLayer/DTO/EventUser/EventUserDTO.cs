c23980e0d9a4dc91c15445fc62635904f0dac04b
Roman <brych.126@gmail.com>
2020-05-27 03:02:54 +0300

    BR: Finish with EventUser

--- EPlast/EPlast.BussinessLayer/DTO/EventUser/EventUserDTO.cs
+++ EPlast/EPlast.BussinessLayer/DTO/EventUser/EventUserDTO.cs
@@ -7,9 +7,6 @@ namespace EPlast.BussinessLayer.DTO.EventUser
     public class EventUserDTO
     {
         public UserDTO User { get; set; }
-        public int CreatedEventCount { get; set; }
-        public int VisitedEventsCount { get; set; }
-        public int PlanedEventCount { get; set; }
         public ICollection<EventGeneralInfoDTO> PlanedEvents { get; set; }
         public ICollection<EventGeneralInfoDTO> CreatedEvents { get; set; }
         public ICollection<EventGeneralInfoDTO> VisitedEvents { get; set; }
