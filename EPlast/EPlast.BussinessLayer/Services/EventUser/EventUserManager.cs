8a0e82328ad7d4bba9090ebf2599303236cb351c
Roman <brych.126@gmail.com>
2020-05-28 14:01:21 +0300

    BR: add usings

--- EPlast/EPlast.BussinessLayer/Services/EventUser/EventUserManager.cs
+++ EPlast/EPlast.BussinessLayer/Services/EventUser/EventUserManager.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using EPlast.BussinessLayer.DTO;
+using EPlast.BussinessLayer.DTO.UserProfiles;
 using Microsoft.EntityFrameworkCore;
 
 namespace EPlast.BussinessLayer.Services.EventUser
