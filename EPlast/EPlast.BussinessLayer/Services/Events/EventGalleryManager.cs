b2663bbe88f4bd119ac845ddbea1c6b06219c805
Roman <brych.126@gmail.com>
2020-06-03 01:46:15 +0300

    BR: add img disposing in eventgallery manager

--- EPlast/EPlast.BussinessLayer/Services/Events/EventGalleryManager.cs
+++ EPlast/EPlast.BussinessLayer/Services/Events/EventGalleryManager.cs
@@ -1,5 +1,5 @@
 ﻿using EPlast.BussinessLayer.Interfaces.Events;
-using EPlast.DataAccess.Entities;
+using EPlast.DataAccess.Entities.Event;
 using EPlast.DataAccess.Repositories;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using EPlast.DataAccess.Entities.Event;
 
 namespace EPlast.BussinessLayer.Services.Events
 {
@@ -32,14 +31,16 @@ namespace EPlast.BussinessLayer.Services.Events
                 {
                     if (file != null && file.Length > 0)
                     {
-                        var img = Image.FromStream(file.OpenReadStream());
-                        var uploads = Path.Combine(_env.WebRootPath, "images\\EventsGallery");
-                        var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
-                        var filePath = Path.Combine(uploads, fileName);
-                        img.Save(filePath);
-                        var gallery = new Gallary() { GalaryFileName = fileName };
-                        _repoWrapper.Gallary.Create(gallery);
-                        _repoWrapper.EventGallary.Create(new EventGallary { EventID = id, Gallary = gallery });
+                        using (var img = Image.FromStream(file.OpenReadStream()))
+                        {
+                            var uploads = Path.Combine(_env.WebRootPath, "images\\EventsGallery");
+                            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                            var filePath = Path.Combine(uploads, fileName);
+                            img.Save(filePath);
+                            var gallery = new Gallary() { GalaryFileName = fileName };
+                            _repoWrapper.Gallary.Create(gallery);
+                            _repoWrapper.EventGallary.Create(new EventGallary { EventID = id, Gallary = gallery });
+                        }
                     }
                 }
                 _repoWrapper.Save();
