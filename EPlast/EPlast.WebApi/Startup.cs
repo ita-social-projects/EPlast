e9dd96d08b1348bdf28be179c2cff4c469314fbe
Andrii Shainoha <andrii.shainoha.pz.2018@lpnu.ua>
2020-06-03 23:43:29 +0300

    Added some changes in account controller

--- EPlast/EPlast.WebApi/Startup.cs
+++ EPlast/EPlast.WebApi/Startup.cs
@@ -5,15 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using EPlast.BussinessLayer;
+using EPlast.BussinessLayer.AccessManagers;
+using EPlast.BussinessLayer.AccessManagers.Interfaces;
 using EPlast.BussinessLayer.Interfaces;
+using EPlast.BussinessLayer.Interfaces.City;
+using EPlast.BussinessLayer.Interfaces.Club;
+using EPlast.BussinessLayer.Interfaces.Events;
+using EPlast.BussinessLayer.Interfaces.EventUser;
 using EPlast.BussinessLayer.Interfaces.UserProfiles;
 using EPlast.BussinessLayer.Services;
+using EPlast.BussinessLayer.Services.City;
+using EPlast.BussinessLayer.Services.City.CityAccess;
+using EPlast.BussinessLayer.Services.Club;
+using EPlast.BussinessLayer.Services.Events;
+using EPlast.BussinessLayer.Services.EventUser;
 using EPlast.BussinessLayer.Services.Interfaces;
 using EPlast.BussinessLayer.Services.UserProfiles;
 using EPlast.BussinessLayer.Settings;
 using EPlast.DataAccess;
 using EPlast.DataAccess.Entities;
 using EPlast.DataAccess.Repositories;
+using EPlast.Models.ViewModelInitializations;
+using EPlast.Models.ViewModelInitializations.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -54,24 +67,69 @@ namespace EPlast.WebApi
                     .AddEntityFrameworkStores<EPlastDBContext>()
                     .AddDefaultTokenProviders();
 
+            services.AddLocalization();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyApi", Version = "v1" });
             });
+            services.AddScoped<IHomeService, HomeService>();
             services.AddScoped<IAccountService, AccountService>();
-            services.AddScoped<IUserService, UserService>();
             services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+            services.AddScoped<IEmailConfirmation, EmailConfirmationService>();
+            services.AddScoped<IDecisionVmInitializer, DecisionVmInitializer>();
+            services.AddScoped<CityAccessSettings, CityAccessSettings>();
+            services.AddScoped<ICityAccessService, CityAccessService>();
+            services.AddScoped<ICityMembersService, CityMembersService>();
+            services.AddScoped<IAnnualReportService, AnnualReportService>();
+            services.AddScoped<IPdfService, PdfService>();
+            services.AddScoped<IDecisionService, DecisionService>();
+            services.AddScoped<IDirectoryManager, DirectoryManager>();
+            services.AddScoped<IFileManager, FileManager>();
+            services.AddScoped<IFileStreamManager, FileStreamManager>();
+            services.AddScoped<IUserService, UserService>();
             services.AddScoped<INationalityService, NationalityService>();
-            services.AddScoped<IEducationService, EducationService>();
             services.AddScoped<IReligionService, ReligionService>();
+            services.AddScoped<IEducationService, EducationService>();
             services.AddScoped<IWorkService, WorkService>();
             services.AddScoped<IGenderService, GenderService>();
-            services.AddScoped<IEmailConfirmation, EmailConfirmationService>();
             services.AddScoped<IDegreeService, DegreeService>();
-            services.AddScoped<IUserManagerService, UserManagerService>();
             services.AddScoped<IConfirmedUsersService, ConfirmedUsersService>();
+            services.AddScoped<IUserManagerService, UserManagerService>();
+            services.AddScoped<IAdminService, AdminService>();
+            services.AddScoped<ICItyAdministrationService, CityAdministrationService>();
+            services.AddScoped<ICityService, CityService>();
             services.AddScoped(typeof(ILoggerService<>), typeof(LoggerService<>));
+            services.AddScoped<IClubService, ClubService>();
+            services.AddScoped<IClubAdministrationService, ClubAdministrationService>();
+            services.AddScoped<IClubMembersService, ClubMembersService>();
+            services.AddScoped<ICreateEventVMInitializer, CreateEventVMInitializer>();
+            services.AddScoped<IUserAccessManagerSettings, UserAccessManagerSettings>();
+            services.AddScoped<IUserAccessManager, UserAccessManager>();
+            services.AddScoped<IActionManager, ActionManager>();
+            services.AddScoped<IEventCategoryManager, EventCategoryManager>();
+            services.AddScoped<IEventTypeManager, EventTypeManager>();
+            services.AddScoped<IEventStatusManager, EventStatusManager>();
+            services.AddScoped<IParticipantStatusManager, ParticipantStatusManager>();
+            services.AddScoped<IParticipantManager, ParticipantManager>();
+            services.AddScoped<IEventGalleryManager, EventGalleryManager>();
+            services.AddScoped<IEventUserManager, EventUserManager>();
+            services.AddScoped<IEventAdminManager, EventAdminManager>();
+            services.AddScoped<IDateTimeHelper, DateTimeHelper>();
             services.Configure<EmailServiceSettings>(Configuration.GetSection("EmailServiceSettings"));
+            services.AddLogging();
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.ClientId = Configuration.GetSection("GoogleAuthentication:GoogleClientId").Value;
+                    options.ClientSecret = Configuration.GetSection("GoogleAuthentication:GoogleClientSecret").Value;
+                })
+                .AddFacebook(options =>
+                {
+                    options.AppId = Configuration.GetSection("FacebookAuthentication:FacebookAppId").Value;
+                    options.AppSecret = Configuration.GetSection("FacebookAuthentication:FacebookAppSecret").Value;
+                });
 
             services.Configure<RequestLocalizationOptions>(
             opts =>
