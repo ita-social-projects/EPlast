44b2f881245ba6db71131bc7e592e8122f0c3a7c
atarason <tarasbotkin@gmail.com>
2020-06-10 18:46:15 +0300

    Added new method for async process.

--- EPlast/EPlast.DataAccess/Repositories/Realizations/Base/RepositoryBase.cs
+++ EPlast/EPlast.DataAccess/Repositories/Realizations/Base/RepositoryBase.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
@@ -21,9 +22,50 @@ namespace EPlast.DataAccess.Repositories
             return this.EPlastDBContext.Set<T>().AsNoTracking();
         }
 
+        public async Task<IEnumerable<T>> FindAllAsync(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = this.GetQueryable(includeProperties);
+
+            return await query.ToListAsync();
+        }
+
+        /// <summary>
+        /// Gets a list of entities.
+        /// </summary>
+        /// <param name="includeProperties">A list of submodels that should be included in the search result.</param>
+        /// <returns>A list of all entities (with deffered execution via <see cref="IQueryable">)</returns>
+        private IQueryable<T> GetQueryable(params Expression<Func<T, object>>[] includeProperties) // We can add some filters, static filter, ordering and etc
+        {
+            IQueryable<T> queryable = this.EPlastDBContext.Set<T>().AsNoTracking(); // ?
+            if (includeProperties != null)
+            {
+                queryable = includeProperties.Aggregate(queryable, (current, includeProperty) => current.Include(includeProperty));
+            }
+
+            return queryable;
+        }
+
         public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
         {
             return this.EPlastDBContext.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public async Task<IEnumerable<T>> FindByConditionAsync(Expression<Func<T, bool>> expression)
+        {
+            IQueryable<T> query = this.BuildQuery(expression);
+
+            return await query.ToListAsync();
+        }
+
+        private IQueryable<T> BuildQuery(Expression<Func<T, bool>> predicate) //TODO: add includeProperties, ordering, count or etc
+        {
+            IQueryable<T> query = this.EPlastDBContext.Set<T>().AsNoTracking(); //?
+            if(predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            return query;
         }
 
         public void Create(T entity)
