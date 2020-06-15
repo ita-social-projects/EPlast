5357ec6a33ff27d9252c88215c1a202db502bd87
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-06-15 21:35:58 +0300

    removed mapping, added methods to controller

--- EPlast/EPlast.WebApi/Models/Decision/DecisionViewModel.cs
+++ EPlast/EPlast.WebApi/Models/Decision/DecisionViewModel.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using EPlast.BussinessLayer.DTO;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 
 namespace EPlast.WebApi.Models.Decision
 {
     public class DecisionViewModel
     {
-        public DecisionWrapper DecisionWrapper { get; set; }
+        public DecisionWrapperDTO DecisionWrapper { get; set; }
         public IEnumerable<SelectListItem> OrganizationListItems { get; set; }
         public IEnumerable<SelectListItem> DecisionStatusTypeListItems { get; set; }
-        public IEnumerable<DecisionTarget> DecisionTargets { get; set; }
+        public IEnumerable<DecisionTargetDTO> DecisionTargets { get; set; }
     }
 }
