33851613e28a2258ba77ddc441a9531e66ef0c2d
Roman <brych.126@gmail.com>
2020-06-15 19:22:59 +0300

    BR: Models folder

---
+++ EPlast/EPlast.WebApi/Models/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPlast.WebApi.Models
+{
+    public class TestClass
+    {
+    }
+}
