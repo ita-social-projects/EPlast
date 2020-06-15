cf48458407b0a2bf8cce31a59ca91de73e6a18f3
Andrii Shainoha <andrii.shainoha.pz.2018@lpnu.ua>
2020-06-15 19:34:34 +0300

    Added some changes for return types in account controller

---
+++ EPlast/EPlast.BussinessLayer/Mapping/UserMapping/UserMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EPlast.BussinessLayer.DTO.UserProfiles;
+using EPlast.DataAccess.Entities;
+
+namespace EPlast.BussinessLayer.Mapping.UserMapping
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+        }
+    }
+}
