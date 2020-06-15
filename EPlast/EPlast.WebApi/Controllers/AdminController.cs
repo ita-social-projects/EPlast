43979379842b26142ae566f6ae4e3dc0d283f2b8
VolodymyrVerm <volodymyr33929@gmail.com>
2020-06-15 16:46:15 +0300

    Added admin and user controller to api project

---
+++ EPlast/EPlast.WebApi/Controllers/AdminController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using EPlast.BussinessLayer.DTO;
+using EPlast.BussinessLayer.DTO.City;
+using EPlast.BussinessLayer.Interfaces.City;
+using EPlast.BussinessLayer.Services.Interfaces;
+using EPlast.ViewModels;
+using EPlast.ViewModels.City;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EPlast.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController: ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IUserManagerService _userManagerService;
+        private readonly IAdminService _adminService;
+        private readonly IMapper _mapper;
+        private readonly ICityService _cityService;
+        private readonly ICItyAdministrationService _cityAdministrationService;
+
+        public AdminController(ILogger<AdminController> logger,
+            IUserManagerService userManagerService,
+            IAdminService adminService,
+            IMapper mapper,
+            ICityService cityService,
+            ICItyAdministrationService cityAdministrationService)
+        {
+            _logger = logger;
+            _userManagerService = userManagerService;
+            _adminService = adminService;
+            _mapper = mapper;
+            _cityService = cityService;
+            _cityAdministrationService = cityAdministrationService;
+        }
+        [Authorize]
+        [HttpGet]
+        [Route("UsersTable")]
+        public async Task<IActionResult> UsersTable()
+        {
+            try
+            {
+                var result = _mapper.Map<IEnumerable<UserTableDTO>, IEnumerable<UserTableViewModel>>(await _adminService.UsersTableAsync());
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, $"Exception: {e.Message}");
+                return RedirectToAction("HandleError", "Error");
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("{userId}")]
+        [Route("Edit")]
+        public async Task<IActionResult> Edit(string userId)
+        {
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                var user = await _userManagerService.FindByIdAsync(userId);
+                if (user == null)
+                {
+                    _logger.Log(LogLevel.Error, $"Can`t find the User");
+                    return RedirectToAction("HandleError", "Error", new { code = 404 });
+                }
+                var userRoles = await _userManagerService.GetRolesAsync(user);
+                var allRoles = await _adminService.GetRolesExceptAdminAsync();
+
+                RoleViewModel model = new RoleViewModel
+                {
+                    UserID = user.Id,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return Ok(model);
+            }
+            _logger.Log(LogLevel.Error, $"User, with userId: {userId}, is null");
+            return RedirectToAction("HandleError", "Error", new { code = 404 });
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("Edit")]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                await _adminService.EditAsync(userId, roles);
+                _logger.LogInformation("Successful role change for {0}", userId);
+                return RedirectToAction("UsersTable");
+            }
+            _logger.Log(LogLevel.Error, $"User, with userId: {userId}, is null");
+            return RedirectToAction("HandleError", "Error", new { code = 404 });
+        }
+
+        [HttpGet("{userId}")]
+        [Authorize(Roles = "Admin")]
+        [ActionName("Delete")]
+        [Route("Delete")]
+        public ActionResult ConfirmDelete(string userId)
+        {
+            ViewBag.userId = userId;
+            return PartialView();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Delete")]
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> Delete(string userId)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    await _adminService.DeleteUserAsync(userId);
+                    _logger.LogInformation("Successful delete user {0}", userId);
+                    return RedirectToAction("UsersTable");
+                }
+                _logger.Log(LogLevel.Error, $"User, with userId: {userId}, is null");
+                return RedirectToAction("HandleError", "Error", new { code = 505 });
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, $"Smth went wrong {e.Message}");
+                return RedirectToAction("HandleError", "Error", new { code = 505 });
+            }
+        }
+
+        [HttpGet]
+        [Route("RegionsAdmins")]
+        public async Task<IActionResult> RegionsAdmins()
+        {
+            var model = new RegionsAdminsViewModel
+            {
+                Cities = _mapper.Map<IEnumerable<CityDTO>, IEnumerable<CityViewModel>>(await _cityService.GetAllDTOAsync())
+            };
+            return Ok(model);
+        }
+
+        [HttpGet("{cityId}")]
+        [Route("GetAdmins")]
+        public async Task<IActionResult> GetAdmins(int cityId)
+        {
+            var res = _mapper.Map<IEnumerable<CityAdministrationDTO>, IEnumerable<CityAdministrationViewModel>>(await _cityAdministrationService.GetByCityIdAsync(cityId));
+            return Ok(res);
+        }
+    }
+}
