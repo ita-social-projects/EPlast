d2218cbaaf17a42abe3d2254a286ca932bb4528e
VolodymyrVerm <volodymyr33929@gmail.com>
2020-06-16 14:57:25 +0300

    Add validation to dto

--- EPlast/EPlast.BussinessLayer/DTO/UserProfiles/EducationDTO.cs
+++ EPlast/EPlast.BussinessLayer/DTO/UserProfiles/EducationDTO.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EPlast.BussinessLayer.DTO.UserProfiles
 {
     public class EducationDTO
     {
         public int ID { get; set; }
+
+        [Display(Name = "Місце навчання")]
+        [RegularExpression(@"^[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,51}((\s+|-)[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,51})*$",
+            ErrorMessage = "Місце навчання має містити тільки літери")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Місце навчання повинне складати від 2 до 50 символів")]
         public string PlaceOfStudy { get; set; }
+
+        [Display(Name = "Спеціальність")]
+        [RegularExpression(@"^[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,51}((\s+|-)[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,51})*$",
+            ErrorMessage = "Спеціальність має містити тільки літери")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Спеціальність повинна складати від 3 до 50 символів")]
         public string Speciality { get; set; }
         public IEnumerable<UserProfileDTO> UsersProfiles { get; set; }
     }
--- EPlast/EPlast.BussinessLayer/DTO/UserProfiles/WorkDTO.cs
+++ EPlast/EPlast.BussinessLayer/DTO/UserProfiles/WorkDTO.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EPlast.BussinessLayer.DTO.UserProfiles
 {
     public class WorkDTO
     {
         public int ID { get; set; }
+        [Display(Name = "Місце праці")]
+        [RegularExpression(@"^[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,31}((\s+|-)[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,31})*$",
+            ErrorMessage = "Місце праці має містити тільки літери")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Місце праці повинне складати від 3 до 30 символів")]
         public string PlaceOfwork { get; set; }
+        [Display(Name = "Посада")]
+        [RegularExpression(@"^[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,31}((\s+|-)[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,31})*$",
+            ErrorMessage = "Посада має містити тільки літери")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Посада повинна складати від 3 до 30 символів")]
         public string Position { get; set; }
         public ICollection<UserProfileDTO> UserProfiles { get; set; }
     }
