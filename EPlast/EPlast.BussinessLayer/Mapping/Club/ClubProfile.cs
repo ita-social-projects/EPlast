e61886cf4896e345a5d45be1b13604d61c6fb40f
boyko.joanna <boyko.joanna@gmail.com>
2020-06-17 16:35:58 +0300

    Started creating Club WebAPI Controller

---
+++ EPlast/EPlast.BussinessLayer/Mapping/Club/ClubProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EPlast.BussinessLayer.DTO.Club;
+using EPlast.DataAccess.Entities;
+//using EPlast.ViewModels;
+
+namespace EPlast.BussinessLayer.Mapping
+{
+    public class ClubProfile : Profile
+    {
+        public ClubProfile()
+        {
+            CreateMap<Club, ClubDTO>().ReverseMap();
+            //CreateMap<ClubViewModel, ClubDTO>().ReverseMap();
+        }
+    }
+}
---
+++ EPlast/EPlast.WebApi/Models/Club/ClubProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using EPlast.BussinessLayer.DTO.Club;
+using EPlast.BussinessLayer.DTO.UserProfiles;
+
+namespace EPlast.WebApi.Models.Club
+{
+    public class ClubProfileViewModel
+    {
+        public ClubDTO Club { get; set; }
+        public UserDTO ClubAdmin { get; set; }
+        public List<ClubMembersDTO> Members { get; set; }
+        public List<ClubMembersDTO> Followers { get; set; }
+        public ICollection<ClubAdministrationDTO> ClubAdministration { get; set; }
+        public bool IsCurrentUserClubAdmin { get; set; }
+        public bool IsCurrentUserAdmin { get; set; }
+    }
+}
