ddb459d67060a99d8967edbb84383118ee1b2a2b
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-06-17 17:13:06 +0300

    added cities controller

---
+++ EPlast/EPlast.WebApi/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using EPlast.BussinessLayer.Interfaces.City;
+using EPlast.BussinessLayer.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPlast.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ILoggerService<CitiesController> _logger;
+        private readonly ICityService _cityService;
+        private readonly IMapper _mapper;
+
+        public CitiesController(ILoggerService<CitiesController> logger, ICityService cityService, IMapper mapper)
+        {
+            _logger = logger;
+            _cityService = cityService;
+            _mapper = mapper;
+        }
+
+    }
+}
