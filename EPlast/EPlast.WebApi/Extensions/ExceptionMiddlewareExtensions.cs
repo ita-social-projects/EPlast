55228a1c10fe2e5aae8c7deaf30cf4743a885e4f
Roman <brych.126@gmail.com>
2020-06-20 23:43:21 +0300

    BR: Add global error handling

---
+++ EPlast/EPlast.WebApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using EPlast.BussinessLayer.Services.Interfaces;
+using EPlast.WebApi.Models.ErrorHandling;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPlast.WebApi.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILoggerService<ControllerBase> logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error."
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
