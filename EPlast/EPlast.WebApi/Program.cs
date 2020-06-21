9e6b842d2d23455d0bd1074040d700ad672c48c1
Roman <brych.126@gmail.com>
2020-06-21 22:23:38 +0300

    BR: Configure Logging in WebApi

--- EPlast/EPlast.WebApi/Program.cs
+++ EPlast/EPlast.WebApi/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace EPlast.WebApi
 {
@@ -12,6 +14,11 @@ namespace EPlast.WebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddNLog();
+                })
                 .UseStartup<Startup>();
     }
 }
