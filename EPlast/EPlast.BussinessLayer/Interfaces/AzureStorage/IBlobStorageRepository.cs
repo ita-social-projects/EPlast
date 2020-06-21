2bf4e901c21a1a8d6aa9291a759aa41ce5dc060e
VolodymyrVerm <volodymyr33929@gmail.com>
2020-06-22 01:49:23 +0300

    Configure azure connection string

--- EPlast/EPlast.BussinessLayer/Interfaces/AzureStorage/IBlobStorageRepository.cs
+++ EPlast/EPlast.BussinessLayer/Interfaces/AzureStorage/IBlobStorageRepository.cs
@@ -6,9 +6,7 @@ namespace EPlast.BussinessLayer.Interfaces.AzureStorage
 {
     public interface IBlobStorageRepository
     {
-        // Task<bool> DownloadBlob(string file, string fileExtension);
         Task<bool> DeleteBlobAsync(string blobNameWithExtension, string containerName);
-        //Task<IEnumerable<BlobViewModel>> GetBlobsAsync();
         Task<bool> UploadBlobAsync(IFormFile blobfile, string blobNameWithExtension, string containerNameKey);
         Task<CloudBlockBlob> GetBlobAsync(string blobNameWithExtension, string containerNameKey);
     }
