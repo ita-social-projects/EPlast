a8c2f492a951a37e64037531ffac7e829a518cea
Andrii Shainoha <andrii.shainoha.pz.2018@lpnu.ua>
2020-06-24 13:27:55 +0300

    Added authoperationfilter for swagger gen

---
+++ EPlast/EPlast.BussinessLayer/Filters/AuthOperationFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPlast.BussinessLayer.Filters
+{
+    public class AuthOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+
+            var isAuthorized = context.MethodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() ||
+                              context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
+
+            if (!isAuthorized) return;
+
+            operation.Responses.TryAdd("401", new OpenApiResponse { Description = "Unauthorized" });
+            operation.Responses.TryAdd("403", new OpenApiResponse { Description = "Forbidden" });
+
+            var jwtbearerScheme = new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearer" }
+            };
+
+            operation.Security = new List<OpenApiSecurityRequirement>
+                {
+                    new OpenApiSecurityRequirement
+                    {
+                        [ jwtbearerScheme ] = new string [] { }
+                    }
+                };
+        }
+    }
+}
