2b9e3f426a777f475b2d6660f1a26b3fc9493031
Andrii Shainoha <andrii.shainoha.pz.2018@lpnu.ua>
2020-06-25 16:14:32 +0300

    Added swagger gen and swagger ui in EPlast solution

--- EPlast/EPlast.WebApi/Startup.cs
+++ EPlast/EPlast.WebApi/Startup.cs
@@ -41,9 +41,7 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 
 namespace EPlast.WebApi
@@ -86,11 +84,8 @@ namespace EPlast.WebApi
                     Description = "JWT Authorization header using the Bearer scheme.",
                 });
                 c.OperationFilter<AuthOperationFilter>();
-
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                //c.IncludeXmlComments(xmlPath);
             });
+
             services.ConfigureSwaggerGen(options =>
             {
                 options.CustomSchemaIds(x => x.FullName);
