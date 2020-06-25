d580692d4add564328540a1436a69f3a1144f8cf
ABashko <bashko.zllv@gmail.com>
2020-06-25 19:27:44 +0300

    Created DTOs for statistics

---
+++ EPlast/EPlast.BLL/DTO/Statistics/City.cs
@@ -0,0 +1,11 @@
+﻿namespace EPlast.BLL.DTO.Statistics
+{
+    public class City
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int RegionId { get; set; }
+        public Region Region { get; set; }
+    }
+}
---
+++ EPlast/EPlast.BLL/DTO/Statistics/StatisticsItemIndicator.cs
@@ -0,0 +1,20 @@
+﻿namespace EPlast.BLL.DTO.Statistics
+{
+    public enum StatisticsItemIndicator
+    {
+        NumberOfPtashata,
+        NumberOfNovatstva,
+        NumberOfUnatstva,
+        NumberOfUnatstvaNoname,
+        NumberOfUnatstvaSupporters,
+        NumberOfUnatstvaMembers,
+        NumberOfUnatstvaProspectors,
+        NumberOfUnatstvaSkobVirlyts,
+        NumberOfSenior,
+        NumberOfSeniorPlastynSupporters,
+        NumberOfSeniorPlastynMembers,
+        NumberOfSeigneur,
+        NumberOfSeigneurSupporters,
+        NumberOfSeigneurMembers
+    }
+}
