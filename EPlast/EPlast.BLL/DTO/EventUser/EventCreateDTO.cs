7315160917731ca74aeb8635dd3507acb1255a03
Шутяк Остап <ishutyak2@gmail.com>
2020-06-30 13:24:10 +0300

    Remove and sort usings

--- EPlast/EPlast.BLL/DTO/EventUser/EventCreateDTO.cs
+++ EPlast/EPlast.BLL/DTO/EventUser/EventCreateDTO.cs
@@ -1,13 +1,11 @@
 ﻿using EPlast.BLL.DTO.Events;
 using System.Collections.Generic;
-using EPlast.BLL.DTO.UserProfiles;
 
 namespace EPlast.BLL.DTO.EventUser
 {
     public class EventCreateDTO
     {
         public EventCreationDTO Event { get; set; }
-        //public CreateEventAdminDTO EventAdmin { get; set; }
         public EventAdministrationDTO Сommandant { get; set; }
         public EventAdministrationDTO Alternate { get; set; }
         public EventAdministrationDTO Bunchuzhnyi { get; set; }
--- EPlast/EPlast.BLL/DTO/EventUser/UserInfoDTO.cs
+++ EPlast/EPlast.BLL/DTO/EventUser/UserInfoDTO.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EPlast.BLL.DTO.EventUser
+﻿namespace EPlast.BLL.DTO.EventUser
 {
     public class UserInfoDTO
     {
