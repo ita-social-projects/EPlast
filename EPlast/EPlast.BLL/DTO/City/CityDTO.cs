e8d95c42734aaa8c9a3a824196370519a0d76442
Ivan Danylevych <3wander3@gmail.com>
2020-07-01 20:43:09 +0300

    Replaced RegionDTO

--- EPlast/EPlast.BLL/DTO/City/CityDTO.cs
+++ EPlast/EPlast.BLL/DTO/City/CityDTO.cs
@@ -34,7 +34,7 @@ namespace EPlast.BLL.DTO.City
 
         [MaxLength(7, ErrorMessage = "Поштовий індекс станиці не має перевищувати 7 символів")]
         public string PostIndex { get; set; }
-        public Region Region { get; set; }
+        public RegionDTO Region { get; set; }
 
         [StringLength(int.MaxValue, MinimumLength = 3)]
         public string Logo { get; set; }
---
+++ EPlast/EPlast.BLL/DTO/Region/RegionAdministrationDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EPlast.BLL.DTO.Admin;
+using EPlast.BLL.DTO.UserProfiles;
+
+namespace EPlast.BLL.DTO.Region
+{
+    public class RegionAdministrationDTO
+    {
+        public int ID { get; set; }
+        public AdminTypeDTO AdminType { get; set; }
+        public UserDTO User { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        //public RegionDTO Region { get; set; }
+    }
+}
