d88d60f6f1c1c3035c6ee6d70a4b69784ff19034
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-07-02 15:40:27 +0300

    added method for creating new decision view model

--- EPlast/EPlast.WebApi/Models/Decision/DecisionViewModel.cs
+++ EPlast/EPlast.WebApi/Models/Decision/DecisionViewModel.cs
@@ -1,6 +1,9 @@
-﻿using EPlast.BLL.DTO;
+﻿using EPlast.BLL;
+using EPlast.BLL.DTO;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EPlast.WebApi.Models.Decision
 {
@@ -10,5 +13,25 @@ namespace EPlast.WebApi.Models.Decision
         public IEnumerable<SelectListItem> OrganizationListItems { get; set; }
         public IEnumerable<SelectListItem> DecisionStatusTypeListItems { get; set; }
         public IEnumerable<DecisionTargetDTO> DecisionTargets { get; set; }
+        public static async Task<DecisionViewModel> GetNewDecisionViewModel(IDecisionService decisionService)
+        {
+            var organizations = await decisionService.GetOrganizationListAsync();
+
+            DecisionViewModel decisionViewModel = new DecisionViewModel
+            {
+                DecisionWrapper = await decisionService.CreateDecisionAsync(),
+                OrganizationListItems = from item in organizations
+                                        select new SelectListItem
+                                        {
+                                            Text = item.OrganizationName,
+                                            Value = item.ID.ToString()
+                                        },
+                DecisionTargets = await decisionService.GetDecisionTargetListAsync(),
+                DecisionStatusTypeListItems = decisionService.GetDecisionStatusTypes()
+            };
+
+            return decisionViewModel;
+        }
+
     }
 }
