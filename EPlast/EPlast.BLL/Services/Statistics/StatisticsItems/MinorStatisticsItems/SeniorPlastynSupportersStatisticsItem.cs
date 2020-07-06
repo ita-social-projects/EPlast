6de9c8518733ceebc8ab8c5708d861b9ebfd3f94
ABashko <bashko.zllv@gmail.com>
2020-07-06 12:32:41 +0300

    Created service to get statistics about cities and regions

---
+++ EPlast/EPlast.BLL/Services/Statistics/StatisticsItems/MinorStatisticsItems/SeniorPlastynSupportersStatisticsItem.cs
@@ -0,0 +1,34 @@
+﻿using EPlast.BLL.DTO.Statistics;
+using EPlast.BLL.Services.Statistics.StatisticsItems.Interfaces;
+using EPlast.DataAccess.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EPlast.BLL.Services.Statistics.StatisticsItems.MinorStatisticsItems
+{
+    public class SeniorPlastynSupportersStatisticsItem : IStatisticsItem
+    {
+        public StatisticsItem GetValue(MembersStatistic membersStatistic)
+        {
+            return new StatisticsItem
+            {
+                Indicator = StatisticsItemIndicator.NumberOfSeniorPlastynSupporters,
+                Value = (membersStatistic == null) ? 0 : membersStatistic.NumberOfSeniorPlastynSupporters
+            };
+        }
+
+        public StatisticsItem GetValue(IEnumerable<MembersStatistic> membersStatistics)
+        {
+            int value = 0;
+            foreach (var membersStatistic in membersStatistics ?? Enumerable.Empty<MembersStatistic>())
+            {
+                value += membersStatistic.NumberOfSeniorPlastynSupporters;
+            }
+            return new StatisticsItem
+            {
+                Indicator = StatisticsItemIndicator.NumberOfSeniorPlastynSupporters,
+                Value = value
+            };
+        }
+    }
+}
---
+++ EPlast/EPlast.XUnitTest/Services/Statistics/StatisticsServiceTests.cs
@@ -0,0 +1,6 @@
+﻿namespace EPlast.XUnitTest.Services.Statistics
+{
+    public class StatisticsServiceTests
+    {
+    }
+}
