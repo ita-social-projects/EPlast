78300fa363b004c82012fcfa205a832ded821fbf
Ivan Danylevych <3wander3@gmail.com>
2020-07-07 15:56:44 +0300

    Removed unnecessary usings

--- EPlast/EPlast.WebApi/Models/City/CityUserViewModel.cs
+++ EPlast/EPlast.WebApi/Models/City/CityUserViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace EPlast.WebApi.Models.City
+﻿namespace EPlast.WebApi.Models.City
 {
     public class CityUserViewModel
     {
