e9ab9f94999cb405b529a92375b1a710e739f0e5
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-07-07 17:07:15 +0300

    created  decision create view model

--- EPlast/EPlast.WebApi/Models/Decision/DecisionViewModel.cs
+++ EPlast/EPlast.WebApi/Models/Decision/DecisionViewModel.cs
@@ -1,10 +1,4 @@
-﻿using EPlast.BLL;
-using EPlast.BLL.DTO;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace EPlast.WebApi.Models.Decision
 {
@@ -24,29 +18,5 @@ namespace EPlast.WebApi.Models.Decision
         public DateTime Date { get; set; }
 
         public string FileName { get; set; }
-        public DecisionWrapperDTO DecisionWrapper { get; set; }
-        public IEnumerable<SelectListItem> OrganizationListItems { get; set; }
-        public IEnumerable<SelectListItem> DecisionStatusTypeListItems { get; set; }
-        public IEnumerable<DecisionTargetDTO> DecisionTargets { get; set; }
-        public static async Task<DecisionViewModel> GetNewDecisionViewModel(IDecisionService decisionService)
-        {
-            var organizations = await decisionService.GetOrganizationListAsync();
-
-            DecisionViewModel decisionViewModel = new DecisionViewModel
-            {
-                DecisionWrapper = await decisionService.CreateDecisionAsync(),
-                OrganizationListItems = from item in organizations
-                                        select new SelectListItem
-                                        {
-                                            Text = item.OrganizationName,
-                                            Value = item.ID.ToString()
-                                        },
-                DecisionTargets = await decisionService.GetDecisionTargetListAsync(),
-                DecisionStatusTypeListItems = decisionService.GetDecisionStatusTypes()
-            };
-
-            return decisionViewModel;
-        }
-
     }
 }
