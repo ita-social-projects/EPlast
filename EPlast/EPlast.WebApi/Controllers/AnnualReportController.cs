3ded7ad842ebe2d269b9dd0685018fbbed3e8923
ABashko <bashko.zllv@gmail.com>
2020-07-16 21:43:01 +0300

    Created resources file for annual report api controller

--- EPlast/EPlast.WebApi/Controllers/AnnualReportController.cs
+++ EPlast/EPlast.WebApi/Controllers/AnnualReportController.cs
@@ -1,53 +1,60 @@
 ﻿using EPlast.BLL.DTO.AnnualReport;
+using EPlast.BLL.Interfaces.Logging;
 using EPlast.BLL.Services.Interfaces;
+using EPlast.Resources;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Threading.Tasks;
-using EPlast.BLL.Interfaces.Logging;
 
 namespace EPlast.WebApi.Controllers
 {
     [ApiController, Route("api/[controller]")]
-    [Authorize(Roles = "Admin, Голова Округу")]
+    [Authorize(Roles = "Admin, Голова Округу, Голова Станиці")]
     public class AnnualReportController : ControllerBase
     {
         private readonly IAnnualReportService _annualReportService;
         private readonly ILoggerService<AnnualReportController> _loggerService;
+        private readonly IStringLocalizer<AnnualReportControllerMessage> _localizer;
 
-        public AnnualReportController(IAnnualReportService annualReportService, ILoggerService<AnnualReportController> loggerService)
+        public AnnualReportController(IAnnualReportService annualReportService, ILoggerService<AnnualReportController> loggerService,
+            IStringLocalizer<AnnualReportControllerMessage> localizer)
         {
             _annualReportService = annualReportService;
             _loggerService = loggerService;
+            _localizer = localizer;
         }
 
         [HttpGet]
+        [Authorize(Roles = "Admin, Голова Округу")]
         public async Task<IActionResult> Get()
         {
-            return Ok(await _annualReportService.GetAllAsync(User));
+            return StatusCode(StatusCodes.Status200OK, new { annualReports = await _annualReportService.GetAllAsync(User) });
         }
 
         [HttpGet("{id:int}")]
+        [Authorize(Roles = "Admin, Голова Округу")]
         public async Task<IActionResult> Get(int id)
         {
             try
             {
-                return Ok(await _annualReportService.GetByIdAsync(User, id));
+                return StatusCode(StatusCodes.Status200OK, new { annualreport = await _annualReportService.GetByIdAsync(User, id) });
             }
-            catch (NullReferenceException e)
+            catch (NullReferenceException)
             {
-                _loggerService.LogError(e.Message);
-                return NotFound(e.Message);
+                _loggerService.LogError(_localizer["NotFound"]);
+                return StatusCode(StatusCodes.Status404NotFound, new { message = _localizer["NotFound"] });
             }
-            catch (UnauthorizedAccessException e)
+            catch (UnauthorizedAccessException)
             {
-                _loggerService.LogError(e.Message);
-                return Forbid(e.Message);
+                _loggerService.LogError(_localizer["NoAccess"]);
+                return StatusCode(StatusCodes.Status403Forbidden, new { message = _localizer["NoAccess"] });
             }
         }
 
         [HttpPost]
-        [Authorize(Roles = "Admin, Голова Округу, Голова Станиці")]
         public async Task<IActionResult> Create(AnnualReportDTO annualReport)
         {
             if (ModelState.IsValid)
@@ -55,17 +62,17 @@ namespace EPlast.WebApi.Controllers
                 try
                 {
                     await _annualReportService.CreateAsync(User, annualReport);
-                    return Ok();
+                    return StatusCode(StatusCodes.Status201Created, new { message = _localizer["Created"] });
                 }
                 catch (InvalidOperationException e)
                 {
                     _loggerService.LogError(e.Message);
-                    return BadRequest(e.Message);
+                    return StatusCode(StatusCodes.Status400BadRequest, new { message = e.Message });
                 }
-                catch (UnauthorizedAccessException e)
+                catch (UnauthorizedAccessException)
                 {
-                    _loggerService.LogError(e.Message);
-                    return Forbid(e.Message);
+                    _loggerService.LogError(_localizer["NoAccess"]);
+                    return StatusCode(StatusCodes.Status403Forbidden, new { message = _localizer["NoAccess"] });
                 }
             }
             else
@@ -75,6 +82,7 @@ namespace EPlast.WebApi.Controllers
         }
 
         [HttpPut]
+        [Authorize(Roles = "Admin, Голова Округу")]
         public async Task<IActionResult> Edit(AnnualReportDTO annualReport)
         {
             if (ModelState.IsValid)
@@ -82,22 +90,22 @@ namespace EPlast.WebApi.Controllers
                 try
                 {
                     await _annualReportService.CreateAsync(User, annualReport);
-                    return Ok();
+                    return StatusCode(StatusCodes.Status200OK, new { message = _localizer["Edited"] });
                 }
-                catch (InvalidOperationException e)
+                catch (InvalidOperationException)
                 {
-                    _loggerService.LogError(e.Message);
-                    return BadRequest(e.Message);
+                    _loggerService.LogError(_localizer["InvalidEdit"]);
+                    return StatusCode(StatusCodes.Status400BadRequest, new { message = _localizer["FailedEdit"] });
                 }
-                catch (NullReferenceException e)
+                catch (NullReferenceException)
                 {
-                    _loggerService.LogError(e.Message);
-                    return NotFound(e.Message);
+                    _loggerService.LogError(_localizer["NotFound"]);
+                    return StatusCode(StatusCodes.Status404NotFound, new { message = _localizer["NotFound"] });
                 }
-                catch (UnauthorizedAccessException e)
+                catch (UnauthorizedAccessException)
                 {
-                    _loggerService.LogError(e.Message);
-                    return Forbid(e.Message);
+                    _loggerService.LogError(_localizer["NoAccess"]);
+                    return StatusCode(StatusCodes.Status403Forbidden, new { message = _localizer["NoAccess"] });
                 }
             }
             else
@@ -107,62 +115,65 @@ namespace EPlast.WebApi.Controllers
         }
 
         [HttpPut("confirm/{id:int}")]
+        [Authorize(Roles = "Admin, Голова Округу")]
         public async Task<IActionResult> Confirm(int id)
         {
             try
             {
                 await _annualReportService.ConfirmAsync(User, id);
-                return Ok();
+                return StatusCode(StatusCodes.Status200OK, new { message = _localizer["Confirmed"] });
             }
-            catch (NullReferenceException e)
+            catch (NullReferenceException)
             {
-                _loggerService.LogError(e.Message);
-                return NotFound(e.Message);
+                _loggerService.LogError(_localizer["NotFound"]);
+                return StatusCode(StatusCodes.Status404NotFound, new { message = _localizer["NotFound"] });
             }
-            catch (UnauthorizedAccessException e)
+            catch (UnauthorizedAccessException)
             {
-                _loggerService.LogError(e.Message);
-                return Forbid(e.Message);
+                _loggerService.LogError(_localizer["NoAccess"]);
+                return StatusCode(StatusCodes.Status403Forbidden, new { message = _localizer["NoAccess"] });
             }
         }
 
         [HttpPut("cancel/{id:int}")]
+        [Authorize(Roles = "Admin, Голова Округу")]
         public async Task<IActionResult> Cancel(int id)
         {
             try
             {
                 await _annualReportService.CancelAsync(User, id);
-                return Ok();
+                return StatusCode(StatusCodes.Status200OK, new { message = _localizer["Canceled"] });
             }
-            catch (NullReferenceException e)
+            catch (NullReferenceException)
             {
-                _loggerService.LogError(e.Message);
-                return NotFound(e.Message);
+                _loggerService.LogError(_localizer["NotFound"]);
+                return StatusCode(StatusCodes.Status404NotFound, new { message = _localizer["NotFound"] });
             }
-            catch (UnauthorizedAccessException e)
+            catch (UnauthorizedAccessException)
             {
-                _loggerService.LogError(e.Message);
-                return Forbid(e.Message);
+                _loggerService.LogError(_localizer["NoAccess"]);
+                return StatusCode(StatusCodes.Status403Forbidden, new { message = _localizer["NoAccess"] });
             }
         }
 
         [HttpDelete("{id:int}")]
+        [Authorize(Roles = "Admin, Голова Округу")]
         public async Task<IActionResult> Delete(int id)
         {
             try
             {
                 await _annualReportService.DeleteAsync(User, id);
-                return Ok();
+                return StatusCode(StatusCodes.Status200OK, new { message = _localizer["Deleted"] });
             }
-            catch (NullReferenceException e)
+            catch (NullReferenceException)
             {
-                _loggerService.LogError(e.Message);
-                return NotFound(e.Message);
+                _loggerService.LogError(_localizer["NotFound"]);
+                return StatusCode(StatusCodes.Status404NotFound, new { message = _localizer["NotFound"] });
             }
-            catch (UnauthorizedAccessException e)
+            catch (UnauthorizedAccessException)
             {
-                _loggerService.LogError(e.Message);
-                return Forbid(e.Message);
+                _loggerService.LogError(_localizer["NoAccess"]);
+                return StatusCode(StatusCodes.Status403Forbidden, new { message = _localizer["NoAccess"] });
             }
         }
     }
--- EPlast/EPlast.XUnitTest/Services/AnnualReport/AnnualReportServiceTests.cs
+++ EPlast/EPlast.XUnitTest/Services/AnnualReport/AnnualReportServiceTests.cs
@@ -21,11 +21,8 @@ namespace EPlast.XUnitTest.Services.AnnualReport
 {
     public class AnnualReportServiceTests
     {
-        private const string ErrorMessageNoAccess = "Ви не маєте доступу до даного звіту!";
         private const string ErrorMessageHasCreated = "Річний звіт для даної станиці вже створений!";
         private const string ErrorMessageHasUnconfirmed = "Станиця має непідтверджені звіти!";
-        private const string ErrorMessageEditFailed = "Не вдалося редагувати річний звіт!";
-        private const string ErrorMessageNotFound = "Не вдалося знайти річний звіт!";
 
         private readonly Mock<IRepositoryWrapper> _repositoryWrapper = new Mock<IRepositoryWrapper>();
         private readonly Mock<ICityAccessService> _cityAccessService = new Mock<ICityAccessService>();
@@ -88,7 +85,6 @@ namespace EPlast.XUnitTest.Services.AnnualReport
             var result = await Assert.ThrowsAsync<UnauthorizedAccessException>(() => _annualReportService.GetByIdAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<int>()));
 
             // Assert
-            Assert.Equal(ErrorMessageNoAccess, result.Message);
             _mapper.Verify(m => m.Map<DatabaseEntities.AnnualReport, AnnualReportDTO>(It.IsAny<DatabaseEntities.AnnualReport>()), Times.Never);
         }
 
@@ -141,7 +137,6 @@ namespace EPlast.XUnitTest.Services.AnnualReport
             var result = await Assert.ThrowsAsync<UnauthorizedAccessException>(() => _annualReportService.CreateAsync(It.IsAny<ClaimsPrincipal>(), new AnnualReportDTO()));
 
             // Assert
-            Assert.Equal(ErrorMessageNoAccess, result.Message);
             _repositoryWrapper.Verify(r => r.AnnualReports.CreateAsync(It.IsAny<DatabaseEntities.AnnualReport>()), Times.Never);
             _repositoryWrapper.Verify(r => r.SaveAsync(), Times.Never);
         }
@@ -228,7 +223,6 @@ namespace EPlast.XUnitTest.Services.AnnualReport
                 _annualReportService.EditAsync(It.IsAny<ClaimsPrincipal>(), new AnnualReportDTO { Status = AnnualReportStatusDTO.Confirmed }));
 
             // Assert
-            Assert.Equal(ErrorMessageEditFailed, result.Message);
             _repositoryWrapper.Verify(r => r.AnnualReports.Update(It.IsAny<DatabaseEntities.AnnualReport>()), Times.Never);
             _repositoryWrapper.Verify(r => r.SaveAsync(), Times.Never);
         }
@@ -247,7 +241,6 @@ namespace EPlast.XUnitTest.Services.AnnualReport
                 _annualReportService.EditAsync(It.IsAny<ClaimsPrincipal>(), new AnnualReportDTO { Status = AnnualReportStatusDTO.Unconfirmed }));
 
             // Assert
-            Assert.Equal(ErrorMessageNoAccess, result.Message);
             _repositoryWrapper.Verify(r => r.AnnualReports.Update(It.IsAny<DatabaseEntities.AnnualReport>()), Times.Never);
             _repositoryWrapper.Verify(r => r.SaveAsync(), Times.Never);
         }
@@ -525,7 +518,6 @@ namespace EPlast.XUnitTest.Services.AnnualReport
             var result = await Assert.ThrowsAsync<UnauthorizedAccessException>(() => _annualReportService.ConfirmAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<int>()));
 
             // Assert
-            Assert.Equal(ErrorMessageNoAccess, result.Message);
             _repositoryWrapper.Verify(r => r.SaveAsync(), Times.Never);
         }
 
@@ -666,7 +658,6 @@ namespace EPlast.XUnitTest.Services.AnnualReport
             var result = await Assert.ThrowsAsync<UnauthorizedAccessException>(() => _annualReportService.CancelAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<int>()));
 
             // Assert
-            Assert.Equal(ErrorMessageNoAccess, result.Message);
             _repositoryWrapper.Verify(r => r.SaveAsync(), Times.Never);
         }
 
@@ -730,7 +721,6 @@ namespace EPlast.XUnitTest.Services.AnnualReport
             var result = await Assert.ThrowsAsync<UnauthorizedAccessException>(() => _annualReportService.DeleteAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<int>()));
 
             // Assert
-            Assert.Equal(ErrorMessageNoAccess, result.Message);
             _repositoryWrapper.Verify(r => r.AnnualReports.Delete(It.IsAny<DatabaseEntities.AnnualReport>()), Times.Never);
             _repositoryWrapper.Verify(r => r.SaveAsync(), Times.Never);
         }
