4fdf7f1cc41607936e3ca5c4f96c6634cd3480f8
Andrii Shainoha <andrii.shainoha.pz.2018@lpnu.ua>
2020-07-23 13:23:17 +0300

    Fixed some problems

--- EPlast/EPlast.WebApi/Startup.cs
+++ EPlast/EPlast.WebApi/Startup.cs
@@ -35,7 +35,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
