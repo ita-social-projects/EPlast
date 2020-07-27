e94f6b08984a2f170944eb755b21d8e3bb1bbc76
Шутяк Остап <ishutyak2@gmail.com>
2020-07-27 22:18:06 +0300

    Added documentation for IEventCalendarService and EventCalendarService

--- EPlast/EPlast.BLL/Interfaces/EventCalendar/IEventCalendarService.cs
+++ EPlast/EPlast.BLL/Interfaces/EventCalendar/IEventCalendarService.cs
@@ -4,12 +4,28 @@ using System.Threading.Tasks;
 
 namespace EPlast.BLL.Interfaces.EventCalendar
 {
+    /// <summary>
+    /// Implement operation for getting events for each type
+    /// </summary>
     public interface IEventCalendarService
     {
+        /// <summary>
+        /// Get all events of actions type
+        /// </summary>
+        /// <returns>Array of events of actions type</returns>
         Task<List<EventCalendarInfoDTO>> GetAllActions();
 
+
+        /// <summary>
+        /// Get all events of education type
+        /// </summary>
+        /// <returns>Array of events of education type</returns>
         Task<List<EventCalendarInfoDTO>> GetAllEducations();
 
+        /// <summary>
+        /// Get all events of camps type
+        /// </summary>
+        /// <returns>Array of events of camps type</returns>
         Task<List<EventCalendarInfoDTO>> GetAllCamps();
     }
 }
