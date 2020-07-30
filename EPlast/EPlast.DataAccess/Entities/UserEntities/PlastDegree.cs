7dc93f15fd9293c5f5745e32053d0d72b229f886
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-07-30 18:06:39 +0300

    added IPlastDegreeRepository

--- EPlast/EPlast.DataAccess/Entities/UserEntities/PlastDegree.cs
+++ EPlast/EPlast.DataAccess/Entities/UserEntities/PlastDegree.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EPlast.DataAccess.Entities.UserEntities
+﻿namespace EPlast.DataAccess.Entities.UserEntities
 {
     public class PlastDegree
     {
