910c2e06c1081b461ab3101cb0c56cdf4b10f118
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-07-30 18:03:57 +0300

    added new entity  PlastDegree

---
+++ EPlast/EPlast.DataAccess/Entities/UserEntities/PlastDegree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPlast.DataAccess.Entities.UserEntities
+{
+    public class PlastDegree
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
