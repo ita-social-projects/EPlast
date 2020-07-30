1d6f524f731ce856e59465d18c18797bc7294861
Roman <brych.126@gmail.com>
2020-07-31 02:29:56 +0300

    BR: Add documentation to events conntroller and services(except event gallery)

--- EPlast/EPlast.BLL/Interfaces/Events/IEventStatusManager.cs
+++ EPlast/EPlast.BLL/Interfaces/Events/IEventStatusManager.cs
@@ -2,8 +2,16 @@
 
 namespace EPlast.BLL.Interfaces.Events
 {
+    /// <summary>
+    ///  Implements  operations for work with event statuses.
+    /// </summary>
     public interface IEventStatusManager
     {
+        /// <summary>
+        /// Get Id of event status by status name.
+        /// </summary>
+        /// <returns>The Id of specific event status.</returns>
+        /// <param name="statusName">The name of event status</param>
         Task<int> GetStatusIdAsync(string statusName);
     }
 }
