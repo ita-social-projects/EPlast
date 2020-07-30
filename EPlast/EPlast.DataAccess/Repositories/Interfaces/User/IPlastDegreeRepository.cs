e35f796987e9d4a9da419dff4bcf42b6fae5df03
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-07-30 18:06:39 +0300

    added IPlastDegreeRepository

---
+++ EPlast/EPlast.DataAccess/Repositories/Interfaces/User/IPlastDegreeRepository.cs
@@ -0,0 +1,8 @@
+﻿using EPlast.DataAccess.Entities.UserEntities;
+
+namespace EPlast.DataAccess.Repositories.Interfaces.User
+{
+    interface IPlastDegreeRepository: IRepositoryBase<PlastDegree>
+    {
+    }
+}
