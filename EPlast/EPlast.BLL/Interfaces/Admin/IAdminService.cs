ce8ee76851c19e06faeb551099c780b8a4dcaeec
VolodymyrVerm <volodymyr33929@gmail.com>
2020-07-31 00:45:38 +0300

    Add documentation to many services

--- EPlast/EPlast.BLL/Interfaces/Admin/IAdminService.cs
+++ EPlast/EPlast.BLL/Interfaces/Admin/IAdminService.cs
@@ -7,9 +7,29 @@ namespace EPlast.BLL.Services.Interfaces
 {
     public interface IAdminService
     {
+        /// <summary>
+        /// Get all roles except Admin role
+        /// </summary>
+        /// <returns>All roles except Admin role</returns>
         Task<IEnumerable<IdentityRole>> GetRolesExceptAdminAsync();
+
+        /// <summary>
+        /// Edit user roles
+        /// </summary>
+        /// <param name="userId">The id of the user</param>
+        /// <param name="roles">List of new user roles</param>
         Task EditAsync(string userId, List<string> roles);
+
+        /// <summary>
+        /// Delete a user
+        /// </summary>
+        /// <param name="userId">The id of the user, which must be deleted</param>
         Task DeleteUserAsync(string userId);
+
+         /// <summary>
+        /// Get all users with additional information
+        /// </summary>
+        /// <returns>Specify model with all users</returns>
         Task<IEnumerable<UserTableDTO>> UsersTableAsync();
     }
 }
