5b41b95dcec91ce51557850539d85f560ca15cc0
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-07-31 13:01:54 +0300

    added summary for GetDergeesAsync and  IActiveMembershipService

--- EPlast/EPlast.BLL/Interfaces/ActiveMembership/IActiveMembershipService.cs
+++ EPlast/EPlast.BLL/Interfaces/ActiveMembership/IActiveMembershipService.cs
@@ -4,8 +4,15 @@ using System.Threading.Tasks;
 
 namespace EPlast.BLL.Interfaces.ActiveMembership
 {
+    /// <summary>
+    /// Implement  operations for work with active membership
+    /// </summary>
     public interface IActiveMembershipService
     {
+       /// <summary>
+       /// Returns all plast degrees
+       /// </summary>
+       /// <returns>All plast degrees</returns>
         public Task<IEnumerable<PlastDergeeDTO>> GetDergeesAsync();
     }
 }
