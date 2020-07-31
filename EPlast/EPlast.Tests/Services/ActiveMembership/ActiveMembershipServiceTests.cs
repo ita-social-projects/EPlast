015a54b62d4fc5cae8937a690618749daa8982e4
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-07-31 13:13:19 +0300

    added SetUp for ActiveMembershipServiceTests

--- EPlast/EPlast.Tests/Services/ActiveMembership/ActiveMembershipServiceTests.cs
+++ EPlast/EPlast.Tests/Services/ActiveMembership/ActiveMembershipServiceTests.cs
@@ -1,7 +1,24 @@
 ﻿
+using AutoMapper;
+using EPlast.BLL.Services.ActiveMembership;
+using EPlast.DataAccess.Repositories;
+using Moq;
+using NUnit.Framework;
+
 namespace EPlast.Tests.Services.ActiveMembership
 {
+    [TestFixture]
     public class ActiveMembershipServiceTests
     {
+        private ActiveMembershipService _activeMembershipService;
+        private Mock<IRepositoryWrapper> _repoWrapper;
+        private Mock<IMapper> _mapper;
+        [SetUp]
+        public void SetUp()
+        {
+            _mapper = new Mock<IMapper>();
+            _repoWrapper = new Mock<IRepositoryWrapper>();
+            _activeMembershipService = new ActiveMembershipService(_mapper.Object, _repoWrapper.Object);
+        }
     }
 }
