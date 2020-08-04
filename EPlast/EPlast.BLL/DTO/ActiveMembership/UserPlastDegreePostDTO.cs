8e1a2ac81ff9d64ec29b6ddf5d3f4df16c3c2fec
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-08-04 14:27:42 +0300

    added UserPlastDegreePostDTO

---
+++ EPlast/EPlast.BLL/DTO/ActiveMembership/UserPlastDegreePostDTO.cs
@@ -0,0 +1,15 @@
+﻿using EPlast.BLL.DTO.UserProfiles;
+using EPlast.DataAccess.Entities;
+using System;
+
+namespace EPlast.BLL.DTO.ActiveMembership
+{
+    public class UserPlastDegreePostDTO
+    {
+        public int Id { get; set; }
+        public PlastDegree PlastDegree { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime? DateFinish { get; set; }
+        public UserDTO User { get; set; }
+    }
+}
