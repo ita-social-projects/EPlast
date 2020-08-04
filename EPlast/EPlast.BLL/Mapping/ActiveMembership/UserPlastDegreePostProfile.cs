57bd4a971db64bd2ac44a4247cde1d32c64e41af
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-08-04 14:33:06 +0300

    added  UserPlastDegreePostProfile

---
+++ EPlast/EPlast.BLL/Mapping/ActiveMembership/UserPlastDegreePostProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EPlast.BLL.DTO.ActiveMembership;
+using EPlast.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPlast.BLL.Mapping.ActiveMembership
+{
+    public class UserPlastDegreePostProfile: Profile
+    {
+        public UserPlastDegreePostProfile()
+        {
+            CreateMap<UserPlastDegreePostDTO, UserPlastDegree>()
+                .ForMember(upd => upd.PlastDegreeId, updp => updp.MapFrom(f => f.PlastDegree.Id))
+                .ForMember(upd => upd.UserId, updp => updp.MapFrom(f => f.User.Id))
+                .ReverseMap();
+        }
+    }
+}
