32d6d2ee2ef8535abf8a1cfe9713a819e6593dba
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-08-04 15:31:51 +0300

    changed AddPlastDegreeForUserAsync logic

--- EPlast/EPlast.DataAccess/Entities/UserEntities/PlastDegree.cs
+++ EPlast/EPlast.DataAccess/Entities/UserEntities/PlastDegree.cs
@@ -9,19 +9,6 @@ namespace EPlast.DataAccess.Entities
         [Required]
         public string Name { get; set; }
         public ICollection<UserPlastDegree> UserPlastDegrees { get; set; }
-        public override bool Equals(object obj)
-        {
-            PlastDegree plastDegree = obj as PlastDegree;
-            if (plastDegree != null)
-            {
-                return Id == plastDegree.Id && Name == plastDegree.Name;
-            }
 
-            return false;
-        }
-        public override int GetHashCode()
-        {
-            return Id;
-        }
-        }
+    }
 }
