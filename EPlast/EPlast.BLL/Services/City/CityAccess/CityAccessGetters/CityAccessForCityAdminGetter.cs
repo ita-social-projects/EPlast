a59cd6ee168f552fc994bb6e427f52bacf62ef77
Andrii Bashko <bashko.zllv@gmail.com>
2020-08-06 12:25:33 +0300

    Fixed dateTime for CityAccessForCityAdminGetter

--- EPlast/EPlast.BLL/Services/City/CityAccess/CityAccessGetters/CityAccessForCityAdminGetter.cs
+++ EPlast/EPlast.BLL/Services/City/CityAccess/CityAccessGetters/CityAccessForCityAdminGetter.cs
@@ -1,5 +1,6 @@
 ﻿using EPlast.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace EPlast.BLL.Services.City.CityAccess.CityAccessGetters
         public async Task<IEnumerable<DatabaseEntities.City>> GetCities(string userId)
         {
             var cityAdministration = await _repositoryWrapper.CityAdministration.GetFirstOrDefaultAsync(
-                    predicate: c => c.UserId == userId && c.EndDate == null);
+                    predicate: c => c.UserId == userId && (DateTime.Now < c.EndDate || c.EndDate == null));
             return cityAdministration != null ? await _repositoryWrapper.City.GetAllAsync(
                 predicate: c => c.ID == cityAdministration.CityId, include: source => source.Include(c => c.Region))
                 : Enumerable.Empty<DatabaseEntities.City>();
