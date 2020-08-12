758e1124647311fed0ee30942234f1c4b219d314
TonyWebber <artur_olenych@icloud.com>
2020-08-12 15:35:42 +0300

    Added entities Distinction, UserDistinction

---
+++ EPlast/EPlast.DataAccess/Entities/UserEntities/UserDistinction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EPlast.DataAccess.Entities.UserEntities
+{
+    public class UserDistinction
+    {
+        public int Id { get; set; }
+        public int DistinctionId { get; set; }
+        public Distinction Distinction { get; set; }
+        public string Reporter { get; set; }
+        public string Reason { get; set; }
+        public DateTime Date { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+    }
+}
