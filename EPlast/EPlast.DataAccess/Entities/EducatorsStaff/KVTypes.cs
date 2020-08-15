7d261e95a0b3aa8e0787ffd72b724b6272bfb766
Viktor <viktor.kalynenko.00@gmail.com>
2020-08-15 20:28:55 +0300

    Added migration for Kadra Vykhovnykiv

---
+++ EPlast/EPlast.DataAccess/Entities/EducatorsStaff/KVTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EPlast.DataAccess.Entities.EducatorsStaff
+{
+   public  class KVTypes
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required, MaxLength(20, ErrorMessage = "Назва типу кадри має бути не довшою ніж 20 символів")]
+        public string KVTypeName { get; set; }
+    }
+}
