807afe76a8a5288e5283497f3b7ed3e7e5df4c83
TonyWebber <artur_olenych@icloud.com>
2020-08-17 11:23:19 +0300

    Added interfaces, created services

---
+++ EPlast/EPlast.BLL/Services/Distinctions/DistinctionService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPlast.BLL.Services.Distinctions
+{
+    class DistinctionService
+    {
+    }
+}
---
+++ EPlast/EPlast.BLL/Services/Distinctions/UserDistinctionService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EPlast.BLL.Services.Distinctions
+{
+    public class UserDistinctionService : IUserDistinctionService
+    {
+        public UserDistinctionDTO AddUserDistinction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ChangeUserDistinction(UserDistinctionDTO userDistinctionDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUserDistinction(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserDistinctionDTO>> GetAllUsersDistinctionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDistinctionDTO> GetUserDistinction(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
