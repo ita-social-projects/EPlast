44385f1babeddcd7714bd904cb43f0595f447c13
TonyWebber <artur_olenych@icloud.com>
2020-08-17 11:23:19 +0300

    Added interfaces, created services

--- EPlast/EPlast.BLL/Interfaces/Distinctions/IUserDistinctionService.cs
+++ EPlast/EPlast.BLL/Interfaces/Distinctions/IUserDistinctionService.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace EPlast.BLL.Interfaces.Distinctions
+namespace EPlast.BLL
 {
     interface IUserDistinctionService
     {
