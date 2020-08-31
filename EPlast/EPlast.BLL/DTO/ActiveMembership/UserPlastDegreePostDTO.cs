1cb9c9c1e4861bea5d556676025edf07c93fec79
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-08-31 15:29:21 +0300

    added SetPlastDegreeAsCurrent method

--- EPlast/EPlast.BLL/DTO/ActiveMembership/UserPlastDegreePostDTO.cs
+++ EPlast/EPlast.BLL/DTO/ActiveMembership/UserPlastDegreePostDTO.cs
@@ -9,6 +9,7 @@ namespace EPlast.BLL.DTO.ActiveMembership
         public int PlastDegreeId { get; set; }
         public DateTime DateStart { get; set; }
         public DateTime? DateFinish { get; set; }
+        public bool IsCurrent { get; set; }
         public string UserId { get; set; }
     }
 }
