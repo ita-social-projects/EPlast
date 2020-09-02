11103bcc6769dff599bd5e61d8db1cb0f01f6438
Andrii Bashko <bashko.zllv@gmail.com>
2020-09-02 12:56:54 +0300

    Fixed bugs for CityAccessForCityAdminGetter and CityAccessForRegionAdminGetter (added check AdminType)

--- EPlast/EPlast.BLL/Services/City/CityAccess/CityAccessGetters/CityAccessForCityAdminGetter.cs
+++ EPlast/EPlast.BLL/Services/City/CityAccess/CityAccessGetters/CityAccessForCityAdminGetter.cs
@@ -1,4 +1,5 @@
-﻿using EPlast.DataAccess.Repositories;
+﻿using EPlast.DataAccess.Entities;
+using EPlast.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,16 +12,19 @@ namespace EPlast.BLL.Services.City.CityAccess.CityAccessGetters
     public class CityAccessForCityAdminGetter : ICItyAccessGetter
     {
         private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly AdminType _cityAdminType;
 
         public CityAccessForCityAdminGetter(IRepositoryWrapper repositoryWrapper)
         {
             _repositoryWrapper = repositoryWrapper;
+            _cityAdminType = _repositoryWrapper.AdminType.GetFirstAsync(
+                    predicate: a => a.AdminTypeName == "Голова Станиці").Result;
         }
 
         public async Task<IEnumerable<DatabaseEntities.City>> GetCities(string userId)
         {
             var cityAdministration = await _repositoryWrapper.CityAdministration.GetFirstOrDefaultAsync(
-                    predicate: c => c.UserId == userId && (DateTime.Now < c.EndDate || c.EndDate == null));
+                    predicate: c => c.UserId == userId && (DateTime.Now < c.EndDate || c.EndDate == null) && c.AdminTypeId == _cityAdminType.ID);
             return cityAdministration != null ? await _repositoryWrapper.City.GetAllAsync(
                 predicate: c => c.ID == cityAdministration.CityId, include: source => source.Include(c => c.Region))
                 : Enumerable.Empty<DatabaseEntities.City>();
