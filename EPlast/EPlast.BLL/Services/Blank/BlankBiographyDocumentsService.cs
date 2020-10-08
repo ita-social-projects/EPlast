23d46f67db52bc65e7ac42462c645497bc66aa98
DESKTOP-1VNOUHA\Yurii <yuriy.shyshkun@gmail.com>
2020-10-08 16:36:12 +0300

    removed code smells

--- EPlast/EPlast.BLL/Services/Blank/BlankBiographyDocumentsService.cs
+++ EPlast/EPlast.BLL/Services/Blank/BlankBiographyDocumentsService.cs
@@ -5,9 +5,7 @@ using EPlast.BLL.Interfaces.Blank;
 using EPlast.DataAccess.Entities.Blank;
 using EPlast.DataAccess.Repositories;
 using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
