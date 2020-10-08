c6bb414f8afb3bce47fa9d98c051bda26056b8b7
mlabensky <mlabensky1998@gmail.com>
2020-10-08 14:03:49 +0300

    Added UserDatesService andadd new methods to ActiveMembershipController

---
+++ EPlast/EPlast.BLL/Interfaces/ActiveMembership/IUserDatesService.cs
@@ -0,0 +1,32 @@
+﻿using EPlast.BLL.DTO.ActiveMembership;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPlast.BLL.Interfaces.ActiveMembership
+{
+    /// <summary>
+    /// Implement  operations for work with access levels
+    /// </summary>
+    public interface IUserDatesService
+    {
+        /// <summary>
+        /// Returns user dates like Date of entry into a Plast, date of oath and date of finish membership
+        /// </summary>
+        /// <param name="userId">User id</param>
+        /// <returns>All dates of current user</returns>
+        public Task<UserMembershipDatesDTO> GetUserMembershipDatesAsync(string userId);
+
+        /// <summary>
+        /// Returns boolean, if dates is correct and they was changed in DB return true, else false
+        /// </summary>
+        /// <param name="userMembershipDatesDTO"> userMembershipDatesDTO</param>
+        /// <returns>bool if changing is successful</returns>
+        public Task<bool> ChangeUserMembershipDatesAsync(UserMembershipDatesDTO userMembershipDatesDTO);
+
+        public Task<bool> AddDateEntryAsync(string userId);
+    }
+
+
+}
---
+++ EPlast/EPlast.BLL/Services/ActiveMembership/UserDatesService.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using EPlast.BLL.DTO.ActiveMembership;
+using EPlast.BLL.Interfaces.ActiveMembership;
+using EPlast.BLL.Services.Interfaces;
+using EPlast.DataAccess.Entities;
+using EPlast.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPlast.BLL.Services.ActiveMembership
+{
+    public class UserDatesService : IUserDatesService
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepositoryWrapper _repoWrapper;
+        private readonly IUserManagerService _userManagerService;
+        public UserDatesService(IMapper mapper, IRepositoryWrapper repoWrapper, IUserManagerService userManagerService)
+        {
+            _mapper = mapper;
+            _repoWrapper = repoWrapper;
+            _userManagerService = userManagerService;
+        }
+
+        public async Task<bool> ChangeUserMembershipDatesAsync(UserMembershipDatesDTO userMembershipDatesDTO)
+        {
+            bool isChanged = false;
+            var userDto = await _userManagerService.FindByIdAsync(userMembershipDatesDTO.UserId);
+            if (userDto != null)
+            {
+                UserMembershipDates userMembershipDates = await _repoWrapper.UserMembershipDates.GetFirstOrDefaultAsync(umd => umd.UserId == userDto.Id);
+                if(userMembershipDates != null)
+                userMembershipDates.DateEntry = userMembershipDatesDTO.DateEntry;
+                userMembershipDates.DateOath = userMembershipDatesDTO.DateOath;
+                userMembershipDates.DateEnd = userMembershipDatesDTO.DateEnd;
+                _repoWrapper.UserMembershipDates.Update(userMembershipDates);
+                await _repoWrapper.SaveAsync();
+                isChanged = true;
+            }
+            return isChanged;
+        }
+
+        public async Task<UserMembershipDatesDTO> GetUserMembershipDatesAsync(string userId)
+        {
+            var userDto = await _userManagerService.FindByIdAsync(userId);
+
+            if(userDto != null)
+            {
+                UserMembershipDates userMembershipDates = await _repoWrapper.UserMembershipDates.GetFirstOrDefaultAsync(umd => umd.UserId == userId);
+                
+                if (userMembershipDates != null)
+                {
+                    return _mapper.Map<UserMembershipDatesDTO>(userMembershipDates);
+                }
+            }
+            return null;
+        }
+
+        public async Task<bool> AddDateEntryAsync(string userId)
+        {
+            var userDto = await _userManagerService.FindByIdAsync(userId);
+
+            if (userDto != null)
+            {
+                UserMembershipDates userMembershipDates = new UserMembershipDates() 
+                {
+                    UserId = userId, DateEntry = userDto.RegistredOn
+                };
+
+                await _repoWrapper.UserMembershipDates.CreateAsync(userMembershipDates);
+                await _repoWrapper.SaveAsync();
+
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
