42b3e1a734d87c6f294739e371cca08dfccb635e
tetyanakutsko <66511831+tetyanakutsko@users.noreply.github.com>
2020-10-11 01:10:58 +0400

    add city service nunit tests

--- EPlast/EPlast.XUnitTest/Services/City/CityMembersServiceTests.cs
+++ EPlast/EPlast.XUnitTest/Services/City/CityMembersServiceTests.cs
@@ -2,6 +2,9 @@
 using EPlast.BLL.DTO.City;
 using EPlast.BLL.Interfaces.City;
 using EPlast.BLL.Services.City;
+using EPlast.BLL.Services.Interfaces;
+using EPlast.BLL.DTO.UserProfiles;
+using EPlast.DataAccess.Entities;
 using EPlast.DataAccess.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore.Query;
@@ -20,6 +23,8 @@ namespace EPlast.XUnitTest.Services.City
     {
         private readonly Mock<IRepositoryWrapper> _repositoryWrapper = new Mock<IRepositoryWrapper>();
         private readonly Mock<IMapper> _mapper = new Mock<IMapper>();
+        private readonly Mock<ICityService> _cityService;
+        private readonly Mock<IUserManagerService> _userManagerService;
         private readonly ICityMembersService _cityMembersService;
         
         public CityMembersServiceTests()
@@ -41,5 +46,24 @@ namespace EPlast.XUnitTest.Services.City
             // Assert
             _mapper.Verify(m => m.Map<IEnumerable<DatabaseEntities.CityMembers>, IEnumerable<CityMembersDTO>>(It.IsAny<IEnumerable<DatabaseEntities.CityMembers>>()));
         }
+        
+        /*[Fact]
+        public async Task AddFollowerAsyncTest()
+        {
+            //Arrange
+            _cityService.
+                Setup(s => s.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new CityDTO());
+            _userManagerService.Setup(s => s.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(new UserDTO());
+            _repositoryWrapper.Setup(s => s.CityMembers.GetFirstOrDefaultAsync
+            (It.IsAny<Expression<Func<CityMembers, bool>>>(), null))
+                .ReturnsAsync(new CityMembers());
+
+            //Act
+            await _cityMembersService.AddFollowerAsync(It.IsAny<int>(), It.IsAny<string>());
+
+            //Assert
+            _repositoryWrapper.Verify(i => i.CityMembers.CreateAsync(It.IsAny<CityMembers>()), Times.Once());
+            _repositoryWrapper.Verify(i => i.CityMembers.Delete(It.IsAny<CityMembers>()), Times.Once());
+        }*/
     }
 }
