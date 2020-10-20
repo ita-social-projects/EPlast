b96e227a1ea3c484ead216b74c8490c33781d9f8
DESKTOP-1VNOUHA\Yurii <yuriy.shyshkun@gmail.com>
2020-10-20 18:23:55 +0300

    Created Services and controllers for ExtractFromUPU

---
+++ EPlast/EPlast.BLL/Services/Blank/BlankExtractFromUPUDocumentService.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using EPlast.BLL.DTO.Blank;
+using EPlast.BLL.Interfaces.AzureStorage;
+using EPlast.BLL.Interfaces.Blank;
+using EPlast.DataAccess.Entities.Blank;
+using EPlast.DataAccess.Repositories;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPlast.BLL.Services.Blank
+{
+    public class BlankExtractFromUPUDocumentService : IBlankExtractFromUPUDocumentService
+    {
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly IMapper _mapper;
+        private readonly IBlankExtractFromUPUBlobStorageRepository _blankFilesBlobStorage;
+
+        public BlankExtractFromUPUDocumentService(IRepositoryWrapper repositoryWrapper,
+           IMapper mapper,
+           IBlankExtractFromUPUBlobStorageRepository blankFilesBlobStorage)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _mapper = mapper;
+            _blankFilesBlobStorage = blankFilesBlobStorage;
+        }
+
+
+        public async Task<ExtractFromUPUDocumentsDTO> AddDocumentAsync(ExtractFromUPUDocumentsDTO ExtractFromUPUDocumentsDTO)
+        {
+            var fileBase64 = ExtractFromUPUDocumentsDTO.BlobName.Split(',')[1];
+            var extension = "." + ExtractFromUPUDocumentsDTO.FileName.Split('.').LastOrDefault();
+            var fileName = Guid.NewGuid() + extension;
+            await _blankFilesBlobStorage.UploadBlobForBase64Async(fileBase64, fileName);
+            ExtractFromUPUDocumentsDTO.BlobName = fileName;
+
+            var document = _mapper.Map<ExtractFromUPUDocumentsDTO, ExtractFromUPUDocuments>(ExtractFromUPUDocumentsDTO);
+            _repositoryWrapper.ExtractFromUPUDocumentsRepository.Attach(document);
+            await _repositoryWrapper.ExtractFromUPUDocumentsRepository.CreateAsync(document);
+            await _repositoryWrapper.SaveAsync();
+
+            return ExtractFromUPUDocumentsDTO;
+        }
+
+        public async Task DeleteFileAsync(int documentId)
+        {
+            var document = await _repositoryWrapper.ExtractFromUPUDocumentsRepository
+               .GetFirstOrDefaultAsync(d => d.ID == documentId);
+
+            await _blankFilesBlobStorage.DeleteBlobAsync(document.BlobName);
+
+            _repositoryWrapper.ExtractFromUPUDocumentsRepository.Delete(document);
+            await _repositoryWrapper.SaveAsync();
+        }
+
+        public async Task<string> DownloadFileAsync(string fileName)
+        {
+            return await _blankFilesBlobStorage.GetBlobBase64Async(fileName);
+        }
+
+        public async Task<ExtractFromUPUDocumentsDTO> GetDocumentByUserId(string userid)
+        {
+            return _mapper.Map<ExtractFromUPUDocuments, ExtractFromUPUDocumentsDTO>(
+               await _repositoryWrapper.ExtractFromUPUDocumentsRepository.GetFirstOrDefaultAsync(i => i.UserId == userid));
+        }
+    }
+}
