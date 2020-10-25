0f805ef914086c93f2bb9ca4f843e40d4fbd9aa7
tetyanakutsko <66511831+tetyanakutsko@users.noreply.github.com>
2020-10-26 01:40:50 +0400

    Added pagination for events categories. Fixed bugs

--- EPlast/EPlast.BLL/Interfaces/Events/IEventCategoryManager.cs
+++ EPlast/EPlast.BLL/Interfaces/Events/IEventCategoryManager.cs
@@ -17,5 +17,14 @@ namespace EPlast.BLL.Interfaces.Events
         /// <returns>List of event categories of the appropriate event type.</returns>
         /// <param name="eventTypeId">The Id of event type</param>
         Task<IEnumerable<EventCategoryDTO>> GetDTOByEventTypeIdAsync(int eventTypeId);
+
+        /// <summary>
+        /// Get list of event categories by event type Id.
+        /// </summary>
+        /// <returns>List of event categories of the appropriate event type.</returns>
+        /// <param name="eventTypeId">The Id of event type</param>
+        /// <param name="page">A number of the page</param>
+        /// <param name="pageSize">A count of categories to display</param>
+        Task<IEnumerable<EventCategoryDTO>> GetDTOByEventPageAsync(int eventTypeId, int page, int pageSize, string CategoryName = null);
     }
 }
