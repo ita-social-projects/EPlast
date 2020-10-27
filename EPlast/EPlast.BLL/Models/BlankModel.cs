a4b480fe157370734b3eed10702137df0e8a944c
DESKTOP-1VNOUHA\Yurii <yuriy.shyshkun@gmail.com>
2020-10-28 00:34:59 +0200

    Wrote PDF file for blank

--- EPlast/EPlast.BLL/Models/BlankModel.cs
+++ EPlast/EPlast.BLL/Models/BlankModel.cs
@@ -5,9 +5,8 @@ namespace EPlast.BLL
     public class BlankModel
     {
         public User User { get; set; }
-        //public UserProfile UserProfile { get; set; }
-        //public CityMembers CityMembers { get; set; }
-        //public User CityAdmin { get; set; }
-        //public ClubMembers ClubMembers { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public CityMembers CityMembers { get; set; }
+        public ClubMembers ClubMembers { get; set; }
     }
 }
