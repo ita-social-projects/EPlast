a271a941ea7d68516078a2b0c4af060e61241c5a
LanchevychMaxym <lanchevich@gmail.com>
2020-10-28 22:59:18 +0200

    Start fixing of adding new pages

--- EPlast/EPlast.BLL/Services/PDF/Documents/DecisionDocument.cs
+++ EPlast/EPlast.BLL/Services/PDF/Documents/DecisionDocument.cs
@@ -1,5 +1,7 @@
-﻿using EPlast.BLL.ExtensionMethods;
+﻿using System.Collections.Generic;
+using EPlast.BLL.ExtensionMethods;
 using EPlast.DataAccess.Entities;
+using PdfSharp.Drawing.Layout;
 using PdfSharpCore;
 using PdfSharpCore.Drawing;
 using PdfSharpCore.Drawing.Layout;
@@ -22,9 +24,9 @@ namespace EPlast.BLL
 
         public override void SetDocumentBody(PdfPage page, XGraphics gfx)
         {
-            DrawText(gfx, decesion);
+            DrawText(gfx, decesion, page);
         }
-        protected void DrawText(XGraphics gfx, Decesion decesion)
+        protected void DrawText(XGraphics gfx, Decesion decesion, PdfPage page)
         {
             const string fontName = "Times New Roman";
 
@@ -47,21 +49,57 @@ namespace EPlast.BLL
             font = new XFont(fontName, 12, XFontStyle.Regular, options);
             format.Alignment = XStringAlignment.Near;
             format.LineAlignment = XLineAlignment.Center;
-            XRect innerRect = new XRect(60, 300, 510, 250);
+            double innerRectHeight = gfx.MeasureString(decesion.Description, font).Height *
+                                    (gfx.MeasureString(decesion.Description, font).Width / 510);
+            XRect innerRect = new XRect(60, 300, 510, innerRectHeight);//250
+            List<(PdfPage,XGraphics)> pages = new List<(PdfPage, XGraphics)>();
+     
+            double totalHeight = gfx.PageSize.Height;
+            //if (innerRectHeight > totalHeight)
+            //    page.Height = innerRectHeight;
+            while (innerRectHeight > totalHeight)
+            {
+                PdfPage newPage = document.AddPage();
+                totalHeight += gfx.PageSize.Height;
+                XGraphics newGfx = XGraphics.FromPdfPage(newPage);
+                pages.Add((newPage, newGfx));
+            }
             //tf.DrawString();
             //gfx.DrawString(decesion.Description, font, XBrushes.Black, rect, format,tf);
-            tf.DrawString(decesion.Description, font, XBrushes.Black, innerRect, XStringFormats.TopLeft);
+            //while (gfx.MeasureString(decesion.Description, font).Height >= innerRect.Height)
+            //    innerRect.Height += 50;
+
+            ////tf.DrawString(decesion.Description, font, XBrushes.Black, innerRect, XStringFormats.TopLeft);
+            XTextFormatterEx2 tfx = new XTextFormatterEx2(gfx);
+            int lastCharIndex;
+            double neededHeight;
+
+            // Draw the text in a box with the optimal height
+            // (magic: we know that one page is enough).
+            XRect rect2 = new XRect(40, 100, 250, double.MaxValue);
+            //tf.Alignment = ParagraphAlignment.Left;
+            tfx.PrepareDrawString(decesion.Description, font, rect2,
+                out lastCharIndex, out neededHeight);
+            rect2 = new XRect(60, 300, 510, neededHeight);
+            //gfx.DrawRectangle(XBrushes.SeaShell, rect);
+            tfx.DrawString(decesion.Description, font, XBrushes.Black, rect2, XStringFormats.TopLeft);
 
             //gfx.DrawString(text, font, XBrushes.Black, 70, 330, format);
             //format.Alignment = XStringAlignment.Far;
             //text = $"{decesion.Name} від {decesion.Date:dd/MM/yyyy}";
             //font = new XFont(fontName, 14, XFontStyle.Regular, options);
             //gfx.DrawString(text, font, XBrushes.Black, rect, format);
+
             format.Alignment = XStringAlignment.Far;
             format.LineAlignment = XLineAlignment.Far;
             font = new XFont(fontName, 14, XFontStyle.Regular, options);
             //text = $"Поточний статус: {decesion.DecesionStatusType.GetDescription()}";
-            gfx.DrawString($"Поточний статус: {decesion.DecesionStatusType.GetDescription()}", font, XBrushes.Black, rect, format);
+            //while (gfx.MeasureString(decesion.Description, font).Height + 300 > innerRect.Height)
+            //    innerRect.Height += 50;
+            XRect bottomRect = new XRect(60, 530, 510, 50);
+            while (tf.LayoutRectangle.IntersectsWith(bottomRect))
+                bottomRect.Y +=  50;
+            gfx.DrawString($"Поточний статус: {decesion.DecesionStatusType.GetDescription()}", font, XBrushes.Black, rect2, format);
 
         }
     }
