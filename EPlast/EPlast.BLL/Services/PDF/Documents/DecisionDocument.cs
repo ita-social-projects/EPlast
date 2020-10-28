5e347be8d9d3e8f040566a989d8ace2dab484804
LanchevychMaxym <lanchevich@gmail.com>
2020-10-28 11:30:34 +0200

    Improve visualization of pdf

--- EPlast/EPlast.BLL/Services/PDF/Documents/DecisionDocument.cs
+++ EPlast/EPlast.BLL/Services/PDF/Documents/DecisionDocument.cs
@@ -2,6 +2,7 @@
 using EPlast.DataAccess.Entities;
 using PdfSharpCore;
 using PdfSharpCore.Drawing;
+using PdfSharpCore.Drawing.Layout;
 using PdfSharpCore.Pdf;
 
 namespace EPlast.BLL
@@ -28,19 +29,39 @@ namespace EPlast.BLL
             const string fontName = "Times New Roman";
 
             XPdfFontOptions options = new XPdfFontOptions(PdfFontEncoding.Unicode);
-            XFont font = new XFont(fontName, 12, XFontStyle.Regular, options);
+            XFont font = new XFont(fontName, 14, XFontStyle.Regular, options);
 
             XStringFormat format = new XStringFormat();
-            string text = $"{decesion.Description}";
+            //string text = decesion.Description;
+            //string text = FormatDescription(decesion.Description);
+            XTextFormatter tf = new XTextFormatter(gfx);
 
-            gfx.DrawString(text, font, XBrushes.Black, 70, 330, format);
+            XRect rect = new XRect(60, 230, 510, 300);
+            gfx.DrawRectangle(XBrushes.White, rect);
 
-            text = $"{decesion.Name} від {decesion.Date:dd/MM/yyyy}";
+            format.Alignment = XStringAlignment.Far;
+            //format.LineAlignment = XLineAlignment.Center;
+            //text = $"{decesion.Name} від {decesion.Date:dd/MM/yyyy}";
+            gfx.DrawString($"{decesion.Name} від {decesion.Date:dd/MM/yyyy}", font, XBrushes.Black, rect, format);
+
+            font = new XFont(fontName, 12, XFontStyle.Regular, options);
+            format.Alignment = XStringAlignment.Near;
+            format.LineAlignment = XLineAlignment.Center;
+            XRect innerRect = new XRect(60, 300, 510, 250);
+            //tf.DrawString();
+            //gfx.DrawString(decesion.Description, font, XBrushes.Black, rect, format,tf);
+            tf.DrawString(decesion.Description, font, XBrushes.Black, innerRect, XStringFormats.TopLeft);
+
+            //gfx.DrawString(text, font, XBrushes.Black, 70, 330, format);
+            //format.Alignment = XStringAlignment.Far;
+            //text = $"{decesion.Name} від {decesion.Date:dd/MM/yyyy}";
+            //font = new XFont(fontName, 14, XFontStyle.Regular, options);
+            //gfx.DrawString(text, font, XBrushes.Black, rect, format);
+            format.Alignment = XStringAlignment.Far;
+            format.LineAlignment = XLineAlignment.Far;
             font = new XFont(fontName, 14, XFontStyle.Regular, options);
-            gfx.DrawString(text, font, XBrushes.Black, 375, 220, format);
-
-            text = $"Поточний статус: {decesion.DecesionStatusType.GetDescription()}";
-            gfx.DrawString(text, font, XBrushes.Black, 350, 480, format);
+            //text = $"Поточний статус: {decesion.DecesionStatusType.GetDescription()}";
+            gfx.DrawString($"Поточний статус: {decesion.DecesionStatusType.GetDescription()}", font, XBrushes.Black, rect, format);
 
         }
     }
