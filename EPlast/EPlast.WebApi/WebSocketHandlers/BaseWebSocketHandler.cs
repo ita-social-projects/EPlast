fdcc8139dd14565ae7bcce68ba137e273df437f8
mlabensky <mlabensky1998@gmail.com>
2020-10-28 02:28:49 +0200

    Created WebSoket Extension and servises for it

---
+++ EPlast/EPlast.WebApi/WebSocketHandlers/BaseWebSocketHandler.cs
@@ -0,0 +1,50 @@
+﻿using EPlast.BLL.Interfaces.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace EPlast.WebApi.WebSocketHandlers
+{
+
+    public abstract class BaseWebSocketHandler
+    {
+        protected INotificationConnectionManager WebSocketConnectionManager { get; set; }
+
+        public BaseWebSocketHandler(INotificationConnectionManager webSocketConnectionManager)
+        {
+            WebSocketConnectionManager = webSocketConnectionManager;
+        }
+
+        public virtual string OnConnected(string userId, WebSocket socket)
+        {
+            return WebSocketConnectionManager.AddSocket(userId, socket);
+        }
+
+        public virtual async Task OnDisconnected(string userId, string connectionId)
+        {
+            await WebSocketConnectionManager.RemoveSocket(userId, connectionId);
+        }
+
+        public async Task SendMessageAsync(string userId, string message)
+        {
+            await WebSocketConnectionManager.SendMessageAsync(userId, message);
+        }
+
+        public async Task SendMessageToAllAsync(string message)
+        {
+            List<Task> tasks = new List<Task>();
+            foreach (var pair in WebSocketConnectionManager.GetAll())
+            {
+                tasks.AddRange(pair.Value.Where(c => c.webSocket.State == WebSocketState.Open)
+                                         .Select(c => SendMessageAsync(pair.Key, message)));
+                
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        public abstract Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer);
+    }
+}
---
+++ EPlast/EPlast.WebApi/WebSocketHandlers/UserNotificationHandler.cs
@@ -0,0 +1,50 @@
+﻿using EPlast.BLL.DTO.Notification;
+using EPlast.BLL.Interfaces.Notifications;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace EPlast.WebApi.WebSocketHandlers
+{
+    public class UserNotificationHandler : BaseWebSocketHandler
+    {
+
+        public UserNotificationHandler(INotificationConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
+        {
+        }
+
+        public override string OnConnected(string userId, WebSocket socket)
+        {
+            return base.OnConnected(userId, socket);
+        }
+
+        public async Task SendUserNotificationAsync(UserNotificationDTO userNotification)
+        {
+            DefaultContractResolver contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
+
+            await SendMessageAsync(userNotification.OwnerUserId, JsonConvert.SerializeObject(userNotification, 
+                new JsonSerializerSettings
+                {
+                    ContractResolver = contractResolver,
+                    Formatting = Formatting.Indented
+                })
+            );
+        }
+
+        public IEnumerable<string> GetOnlineUsers()
+        {
+            return WebSocketConnectionManager.OnlineUsers;
+        }
+
+        public override Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
