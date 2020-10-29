a4dbc7e80f2ed9f8616e9ff69b9ae5cf6e88d67a
LanchevychMaxym <lanchevich@gmail.com>
2020-10-30 00:07:15 +0200

    Add ability to create many pages

--- EPlast/EPlast.BLL/Services/PDF/Documents/DecisionDocument.cs
+++ EPlast/EPlast.BLL/Services/PDF/Documents/DecisionDocument.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using EPlast.BLL.ExtensionMethods;
 using EPlast.DataAccess.Entities;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using PdfSharp.Drawing.Layout;
 using PdfSharpCore;
 using PdfSharpCore.Drawing;
@@ -52,18 +53,18 @@ namespace EPlast.BLL
             double innerRectHeight = gfx.MeasureString(decesion.Description, font).Height *
                                     (gfx.MeasureString(decesion.Description, font).Width / 510);
             XRect innerRect = new XRect(60, 300, 510, innerRectHeight);//250
-            List<(PdfPage,XGraphics)> pages = new List<(PdfPage, XGraphics)>();
+            //List<(PdfPage,XGraphics)> pages = new List<(PdfPage, XGraphics)>();
      
-            double totalHeight = gfx.PageSize.Height;
+            //double totalHeight = gfx.PageSize.Height;
             //if (innerRectHeight > totalHeight)
             //    page.Height = innerRectHeight;
-            while (innerRectHeight > totalHeight)
-            {
-                PdfPage newPage = document.AddPage();
-                totalHeight += gfx.PageSize.Height;
-                XGraphics newGfx = XGraphics.FromPdfPage(newPage);
-                pages.Add((newPage, newGfx));
-            }
+            //while (innerRectHeight > totalHeight)
+            //{
+            //    PdfPage newPage = document.AddPage();
+            //    totalHeight += gfx.PageSize.Height;
+            //    XGraphics newGfx = XGraphics.FromPdfPage(newPage);
+            //    pages.Add((newPage, newGfx));
+            //}
             //tf.DrawString();
             //gfx.DrawString(decesion.Description, font, XBrushes.Black, rect, format,tf);
             //while (gfx.MeasureString(decesion.Description, font).Height >= innerRect.Height)
@@ -76,31 +77,80 @@ namespace EPlast.BLL
 
             // Draw the text in a box with the optimal height
             // (magic: we know that one page is enough).
-            XRect rect2 = new XRect(40, 100, 250, double.MaxValue);
+            int rectHeight = 200;
+            XRect rect2 = new XRect(60, 300, 510, rectHeight);
             //tf.Alignment = ParagraphAlignment.Left;
-            tfx.PrepareDrawString(decesion.Description, font, rect2,
+            tfx.PrepareDrawString(decesion.Description, font, new XRect(60, 300, 510, double.MaxValue), 
                 out lastCharIndex, out neededHeight);
-            rect2 = new XRect(60, 300, 510, neededHeight);
+            //rect2 = new XRect(60, 300, 510, neededHeight);
+            if (neededHeight> rectHeight)
+            {
+                rect2 = new XRect(60, 300, 510, neededHeight);
+            }
+            if (neededHeight >page.Height- rectHeight-rect2.Y)
+            {
+                rect2 = new XRect(60, 300, 510, page.Height /*- rectHeight*/ - rect2.Y-30);
+                tfx.PrepareDrawString(decesion.Description, font, rect2,
+                    out lastCharIndex, out neededHeight);
+                rect2 = new XRect(60, 300, 510, neededHeight);
+            }
+            //rect2 = new XRect(60, 300, 510, neededHeight);
             //gfx.DrawRectangle(XBrushes.SeaShell, rect);
             tfx.DrawString(decesion.Description, font, XBrushes.Black, rect2, XStringFormats.TopLeft);
+            if (lastCharIndex == -1)
+            {
+                XRect bottomRect = new XRect(60, 10 + rect2.Y+ rect2.Height, 510, 50);
+                format.Alignment = XStringAlignment.Far;
+                format.LineAlignment = XLineAlignment.Far;
+                font = new XFont(fontName, 14, XFontStyle.Regular, options);
+                gfx.DrawString($"Поточний статус: {decesion.DecesionStatusType.GetDescription()}", font, XBrushes.Black,
+                    bottomRect, format);
+            }
+            else
+            {
 
-            //gfx.DrawString(text, font, XBrushes.Black, 70, 330, format);
-            //format.Alignment = XStringAlignment.Far;
-            //text = $"{decesion.Name} від {decesion.Date:dd/MM/yyyy}";
-            //font = new XFont(fontName, 14, XFontStyle.Regular, options);
-            //gfx.DrawString(text, font, XBrushes.Black, rect, format);
+                PdfPage newPage = document.AddPage();
+                DrawNextPage(newPage, decesion.Description.Substring(lastCharIndex+1), font);
+            }
+            
 
-            format.Alignment = XStringAlignment.Far;
-            format.LineAlignment = XLineAlignment.Far;
-            font = new XFont(fontName, 14, XFontStyle.Regular, options);
-            //text = $"Поточний статус: {decesion.DecesionStatusType.GetDescription()}";
-            //while (gfx.MeasureString(decesion.Description, font).Height + 300 > innerRect.Height)
-            //    innerRect.Height += 50;
-            XRect bottomRect = new XRect(60, 530, 510, 50);
-            while (tf.LayoutRectangle.IntersectsWith(bottomRect))
-                bottomRect.Y +=  50;
-            gfx.DrawString($"Поточний статус: {decesion.DecesionStatusType.GetDescription()}", font, XBrushes.Black, rect2, format);
+        }
 
+        void DrawNextPage(PdfPage page, string text, XFont font)
+        {
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XStringFormat format = new XStringFormat();
+            XTextFormatterEx2 tfx = new XTextFormatterEx2(gfx);
+            double newNeedHeight;
+            int newIndexOfLastCharOnPage;
+            var rect = new XRect(60, 40, 510, double.MaxValue);
+            tfx.PrepareDrawString(text, font, rect,
+                out newIndexOfLastCharOnPage, out newNeedHeight);
+            if (newNeedHeight > page.Height - 50)
+            {
+                rect = new XRect(60, 40, 510, page.Height - 50);
+                tfx.PrepareDrawString(text, font, rect,
+                    out newIndexOfLastCharOnPage, out newNeedHeight);
+            }
+            rect = new XRect(60, 40, 510, newNeedHeight);
+
+            tfx.DrawString(text,font, XBrushes.Black,rect,format);
+         
+
+
+            if (newIndexOfLastCharOnPage == -1)
+            {
+                XRect bottomRect = new XRect(60, 10+newNeedHeight, 510, 50);
+                format.Alignment = XStringAlignment.Far;
+                format.LineAlignment = XLineAlignment.Far;
+                font = new XFont(font.Name, 14, XFontStyle.Regular, new XPdfFontOptions(PdfFontEncoding.Unicode));
+                
+                gfx.DrawString($"Поточний статус: {decesion.DecesionStatusType.GetDescription()}", font, XBrushes.Black,
+                    bottomRect, format);
+                return;
+            }
+            PdfPage newPage = document.AddPage();
+            DrawNextPage(newPage, text.Substring(newIndexOfLastCharOnPage+1),font);
         }
     }
 }
