e896a082e25f0d654c2ec093dfffb70acd3a7009
mlabensky <mlabensky1998@gmail.com>
2020-11-19 20:51:04 +0200

    Changed regular exspressions for editing page

--- EPlast/EPlast.WebApi/Models/UserModels/UserProfileFields/DegreeViewModel.cs
+++ EPlast/EPlast.WebApi/Models/UserModels/UserProfileFields/DegreeViewModel.cs
@@ -8,7 +8,6 @@ namespace EPlast.WebApi.Models.UserModels.UserProfileFields
         [Display(Name = "Ступінь")]
         [RegularExpression(@"^[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,31}((\s+|-)[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{1,31})*$",
             ErrorMessage = "Ступінь має містити тільки літери")]
-        //[StringLength(30, MinimumLength = 3, ErrorMessage = "Ступінь повинна складати від 3 до 30 символів")]
         public string Name { get; set; }
     }
 }
