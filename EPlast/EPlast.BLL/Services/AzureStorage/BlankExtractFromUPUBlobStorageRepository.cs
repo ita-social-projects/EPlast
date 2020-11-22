370831c7787411f3d83c9db842f7db387fcdacb0
LanchevychMaxym <lanchevich@gmail.com>
2020-11-23 01:52:51 +0200

    Remove unnesessary usings

--- EPlast/EPlast.BLL/Services/AzureStorage/BlankExtractFromUPUBlobStorageRepository.cs
+++ EPlast/EPlast.BLL/Services/AzureStorage/BlankExtractFromUPUBlobStorageRepository.cs
@@ -2,9 +2,6 @@
 using EPlast.BLL.Interfaces.AzureStorage;
 using EPlast.BLL.Interfaces.AzureStorage.Base;
 using EPlast.BLL.Services.AzureStorage.Base;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.Storage.Blob;
-using System.Threading.Tasks;
 
 namespace EPlast.BLL.Services.AzureStorage
 {
