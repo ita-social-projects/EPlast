982ec97254cbf534cbb24a28a8eebdf92afbd2fe
iryna_bodnar <bodnariryna8@gmail.com>
2020-11-22 11:39:42 +0200

    Fixes for secretaries

--- EPlast/EPlast.BLL/Services/Club/ClubAdministrationService.cs
+++ EPlast/EPlast.BLL/Services/Club/ClubAdministrationService.cs
@@ -142,20 +142,32 @@ namespace EPlast.BLL.Services.Club
 
         public async Task<IEnumerable<ClubAdministrationDTO>> GetAdministrationsOfUserAsync(string UserId)
         {
-            var admins = await _repositoryWrapper.ClubAdministration.GetAllAsync(a => a.UserId == UserId && a.Status,
+            var admins = await _repositoryWrapper.ClubAdministration.GetAllAsync(a => a.UserId == UserId && (a.EndDate > DateTime.Now || a.EndDate == null),
                  include:
                  source => source.Include(c => c.User).Include(a => a.Club).Include(c => c.AdminType)
                  );
+
+            foreach (var admin in admins)
+            {
+                admin.Club.ClubAdministration = null;
+            }
+
             return _mapper.Map<IEnumerable<ClubAdministration>, IEnumerable<ClubAdministrationDTO>>(admins);
         }
 
 
         public async Task<IEnumerable<ClubAdministrationDTO>> GetPreviousAdministrationsOfUserAsync(string UserId)
         {
-            var admins = await _repositoryWrapper.ClubAdministration.GetAllAsync(a => a.UserId == UserId && !a.Status,
+            var admins = await _repositoryWrapper.ClubAdministration.GetAllAsync(a => a.UserId == UserId && a.EndDate < DateTime.Now,
                  include:
                  source => source.Include(c => c.User).Include(c => c.AdminType).Include(c => c.Club)
                  );
+
+            foreach (var admin in admins)
+            {
+                admin.Club.ClubAdministration = null;
+            }
+
             return _mapper.Map<IEnumerable<ClubAdministration>, IEnumerable<ClubAdministrationDTO>>(admins);
         }
 
