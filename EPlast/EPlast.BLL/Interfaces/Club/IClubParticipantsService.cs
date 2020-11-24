aa0efc310d36584219edc521f6120e3e1542fcbb
tetyanakutsko <66511831+tetyanakutsko@users.noreply.github.com>
2020-11-24 15:44:14 +0400

    Fixed club controller. Joined ClubAdministration and ClubMembers to Club Participants.

--- EPlast/EPlast.BLL/Interfaces/Club/IClubParticipantsService.cs
+++ EPlast/EPlast.BLL/Interfaces/Club/IClubParticipantsService.cs
@@ -1,4 +1,4 @@
-﻿using EPlast.BLL.DTO.Club;
+using EPlast.BLL.DTO.Club;
 using EPlast.DataAccess.Entities;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -6,8 +6,51 @@ using System.Threading.Tasks;
 
 namespace EPlast.BLL.Interfaces.Club
 {
-    public interface IClubMembersService
+    public interface IClubParticipantsService
     {
+        /// <summary>
+        /// Get an information about a specific administrator
+        /// </summary>
+        /// <param name="ClubId"></param>
+        /// <returns>An information about a specific administrator</returns>
+        Task<IEnumerable<ClubAdministrationDTO>> GetAdministrationByIdAsync(int ClubId);
+
+        /// <summary>
+        /// Add a new administrator to the Club
+        /// </summary>
+        /// <param name="adminDTO">An information about a new administrator</param>
+        /// <returns>An information about a specific administrator</returns>
+        Task<ClubAdministrationDTO> AddAdministratorAsync(ClubAdministrationDTO adminDTO);
+
+        /// <summary>
+        /// Edit an information about a specific admininstrator
+        /// </summary>
+        /// <param name="adminDTO">An information about an edited administrator</param>
+        /// <returns>An information about a specific admininstrator</returns>
+        Task<ClubAdministrationDTO> EditAdministratorAsync(ClubAdministrationDTO adminDTO);
+
+        /// <summary>
+        /// Remove a specific administrator from the Club
+        /// </summary>
+        /// <param name="adminId">The id of the administrator</param>
+        Task RemoveAdministratorAsync(int adminId);
+
+        /// <summary>
+        /// Removes roles from previous administrators
+        /// </summary>
+        Task CheckPreviousAdministratorsToDelete();
+
+        /// <summary>
+        ///returns administrations of given user
+        /// </summary>
+        Task<IEnumerable<ClubAdministrationDTO>> GetAdministrationsOfUserAsync(string UserId);
+
+        /// <summary>
+        ///returns administrations of given user
+        /// </summary>
+        Task<IEnumerable<ClubAdministrationDTO>> GetPreviousAdministrationsOfUserAsync(string UserId);
+        Task<IEnumerable<ClubAdministrationStatusDTO>> GetAdministrationStatuses(string UserId);
+
         /// <summary>
         /// Get all members by specific Club
         /// </summary>
@@ -52,5 +95,6 @@ namespace EPlast.BLL.Interfaces.Club
         /// </summary>
         /// <param name="member">The member of the club</param>
         Task RemoveMemberAsync(ClubMembers member);
+
     }
 }
