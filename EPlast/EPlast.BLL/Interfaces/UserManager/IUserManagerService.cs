8b91576341d2bd931940dc38bc92eafcf7c60d06
LanchevychMaxym <lanchevich@gmail.com>
2020-12-16 03:04:37 +0200

    Remove comments

--- EPlast/EPlast.BLL/Interfaces/UserManager/IUserManagerService.cs
+++ EPlast/EPlast.BLL/Interfaces/UserManager/IUserManagerService.cs
@@ -8,8 +8,6 @@ namespace EPlast.BLL.Services.Interfaces
     public interface IUserManagerService
     {
         Task<bool> IsInRoleAsync(UserDTO user, params string[] roles);
-        //Task<bool> IsInRoleAsync(ClaimsPrincipal user, params string[] roles);
-        //Task<string> GetUserIdAsync(ClaimsPrincipal user);
         Task<UserDTO> FindByIdAsync(string userId);
         Task<IEnumerable<string>> GetRolesAsync(UserDTO user);
     }
