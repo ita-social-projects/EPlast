20f4dc999fc9643d23bb233b11b32115c50985d8
AlexKashchuk <ky3he4ik@gmail.com>
2020-12-17 19:47:38 +0200

    Revert "rewrite test for RegionAdmin"
    This reverts commit fc956edda56773eb4e640b06ead1f1fa7991f040.

--- EPlast/EPlast.Tests/Services/Regions/RegionAdministrationServiceTests.cs
+++ EPlast/EPlast.Tests/Services/Regions/RegionAdministrationServiceTests.cs
@@ -222,20 +222,7 @@ namespace EPlast.Tests.Services.Regions
                .ReturnsAsync(regionAdm);
             _userManager
                 .Setup(u=>u.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(new User() {Id= "Голова Станиці"});
-            _userManager
-                .Setup(u=>u.AddToRoleAsync(new User() { Id = "Голова Станиці" }, "Діловод Округу"));
-            _userManager
-                 .Setup(u => u.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(new User() { Id = "Голова Станиці" });
-            _userManager
-                .Setup(x=>x.RemoveFromRoleAsync(It.IsAny<User>(), It.IsAny<string>()));
-            _repoWrapper
-                .Setup(x=>x.RegionAdministration.Update(regionAdm));
-            _repoWrapper
-                .Setup(x=>x.SaveAsync());
-            _repoWrapper
-                .Setup(x=>x.RegionAdministration.CreateAsync(regionAdm));
-            _repoWrapper
-               .Setup(x => x.SaveAsync());
+            _userManager.Setup(u=>u.AddToRoleAsync(new User() { Id = "Голова Станиці" }, "Діловод Округу"));
             //Act
             var result = _servise.AddRegionAdministrator(regionAdmDTO);
             //Assert
