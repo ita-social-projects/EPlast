ac1bacc7ae39e9732e498ea4729b5b16454cd207
Solomiia <solomiiachuma@gmail.com>
2021-01-17 19:29:34 +0200

    Added new columns to usertable

--- EPlast/EPlast.DataAccess/Entities/UserEntities/User.cs
+++ EPlast/EPlast.DataAccess/Entities/UserEntities/User.cs
@@ -37,6 +37,7 @@ namespace EPlast.DataAccess.Entities
         public DateTime RegistredOn { get; set; }
         public DateTime EmailSendedOnRegister { get; set; }
         public DateTime EmailSendedOnForgotPassword { get; set; }
+        public string Comment { get; set; }
         public string ImagePath { get; set; }
         public bool SocialNetworking { get; set; }
         public UserProfile UserProfile { get; set; }
@@ -56,6 +57,7 @@ namespace EPlast.DataAccess.Entities
         public ICollection<EducatorsStaff.EducatorsStaff> UsersKadras { get; set; }
         public ICollection<UserDistinction> UserDistinctions { get; set; }
         public ICollection<BlankBiographyDocuments> BlankBiographyDocuments { get; set; }
+     
         public User()
         {
             Approvers = new List<Approver>();
