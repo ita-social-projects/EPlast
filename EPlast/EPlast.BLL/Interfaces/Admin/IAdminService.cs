c68c184418c815fab91e06b30c342cf156d97852
vovikboyko <31668723+vovikboyko@users.noreply.github.com>
2021-01-20 12:21:38 +0200

    Fixed register, added tabs to UserTable

--- EPlast/EPlast.BLL/Interfaces/Admin/IAdminService.cs
+++ EPlast/EPlast.BLL/Interfaces/Admin/IAdminService.cs
@@ -40,12 +40,6 @@ namespace EPlast.BLL.Services.Interfaces
         Task<IEnumerable<UserTableDTO>> UsersTableAsync();
 
         /// <summary>
-        /// Get all users with no confirmed email
-        /// </summary>
-        /// <returns>Specify model with all users</returns>
-        Task<IEnumerable<UserTableDTO>> InactiveUsersTableAsync();
-
-        /// <summary>
         /// Change Current Role of user
         /// </summary>
         Task ChangeCurrentRoleAsync(string userId, string role);
