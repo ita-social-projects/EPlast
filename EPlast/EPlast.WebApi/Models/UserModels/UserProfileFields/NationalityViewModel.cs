e4b3ec26d28fbc5a8790a192d760611d7f8b8ddb
Oleh Hatsko <o.hatsko.leh@gmail.com>
2021-04-07 15:41:24 +0300

    FixedNationality

--- EPlast/EPlast.WebApi/Models/UserModels/UserProfileFields/NationalityViewModel.cs
+++ EPlast/EPlast.WebApi/Models/UserModels/UserProfileFields/NationalityViewModel.cs
@@ -6,7 +6,7 @@ namespace EPlast.WebApi.Models.UserModels.UserProfileFields
     {
         public int ID { get; set; }
         [Display(Name = "Національність")]
-        [RegularExpression(@"^[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{0,26}((\s+|-)[a-zA-Zа-яА-ЯІіЄєЇїҐґ'.`]{0,26})*$",
+        [RegularExpression(@"^[a-zA-Zа-яА-ЯІіЄєЇїҐґ'().`]{0,26}((\s+|-)[a-zA-Zа-яА-ЯІіЄєЇїҐґ'().`]{0,26})*$",
             ErrorMessage = "Національність має містити тільки літери")]
         public string Name { get; set; }
     }
