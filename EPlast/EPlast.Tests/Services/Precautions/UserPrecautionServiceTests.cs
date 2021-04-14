3fcf888081b64462c316029bcf3565bf060cd676
Oleg <olegshnic@gmail.com>
2021-04-14 17:58:00 +0300

    Added tests for AdminController & UserPrecautionService

--- EPlast/EPlast.Tests/Services/Precautions/UserPrecautionServiceTests.cs
+++ EPlast/EPlast.Tests/Services/Precautions/UserPrecautionServiceTests.cs
@@ -19,6 +19,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using EPlast.BLL.DTO;
+using EPlast.BLL.DTO.UserProfiles;
 
 namespace EPlast.Tests.Services.Precautions
 {
@@ -380,12 +381,11 @@ namespace EPlast.Tests.Services.Precautions
             Assert.IsInstanceOf<UserPrecautionDTO>(result);
         }
         [Test]
-        public async Task UsersTableWithotPrecautionAsync_ReturnsIEnumerableUserTableDTO()
+        public async Task UsersTableWithoutPrecautionAsync_ReturnsIEnumerableUserTableDTO()
         {
             // Arrange
-            adminService.Setup(a => a.GetUsersTableAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(),
-                    It.IsAny<IEnumerable<string>>(), It.IsAny<IEnumerable<string>>(), It.IsAny<IEnumerable<string>>(), It.IsAny<IEnumerable<string>>()))
-                .ReturnsAsync(CreateTuple);
+            adminService.Setup(a => a.GetUsersAsync())
+                .ReturnsAsync(GetTestUserTableDTO());
             mockRepoWrapper.Setup(x => x.UserPrecaution.GetAllAsync(It.IsAny<Expression<Func<UserPrecaution, bool>>>(),
                     It.IsAny<Func<IQueryable<UserPrecaution>, IIncludableQueryable<UserPrecaution, object>>>()))
                 .ReturnsAsync(GetTestUserPrecaution());
@@ -456,6 +456,21 @@ namespace EPlast.Tests.Services.Precautions
             Reason = "",
             Reporter = ""
         };
+
+        private IEnumerable<UserTableDTO> GetTestUserTableDTO()
+        {
+            return new List<UserTableDTO>
+            {
+                new  UserTableDTO
+                {
+                    User = new ShortUserInformationDTO { ID = UserId }
+                },
+                new  UserTableDTO
+                {
+                    User = new ShortUserInformationDTO { ID = UserId }
+                }
+            }.AsEnumerable();
+        }
 
         private IEnumerable<UserPrecaution> GetTestUserPrecaution()
         {
