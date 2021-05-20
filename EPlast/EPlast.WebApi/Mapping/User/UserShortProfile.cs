5aca9038996b77ce03b3444da919d486f8cee529
tania <tanyabiryuk7@gmail.com>
2021-05-20 19:05:28 +0300

    fixed link to city/club on profile

--- EPlast/EPlast.WebApi/Mapping/User/UserShortProfile.cs
+++ EPlast/EPlast.WebApi/Mapping/User/UserShortProfile.cs
@@ -20,7 +20,19 @@ namespace EPlast.WebApi.Mapping.User
                 .ForMember(x => x.FacebookLink, q => q.MapFrom(w => w.UserProfile.FacebookLink))
                 .ForMember(x => x.TwitterLink, q => q.MapFrom(w => w.UserProfile.TwitterLink))
                 .ForMember(x => x.InstagramLink, q => q.MapFrom(w => w.UserProfile.InstagramLink))
-                .ReverseMap();
+                .ForMember(x => x.CityId, q => q.MapFrom(w => w.CityMembers.FirstOrDefault().City.ID))
+                .ForMember(x => x.ClubId, q => q.MapFrom(w => w.ClubMembers.FirstOrDefault().Club.ID))
+                .ForMember(x => x.RegionId, q => q.MapFrom(w => w.CityMembers.FirstOrDefault().City.RegionId));
+
+            CreateMap<UserViewModel, UserDTO>()
+                    .ForPath(x => x.UserProfile.ID, q => q.MapFrom(w => w.UserProfileID))
+                    .ForPath(x => x.UserProfile.UserID, q => q.MapFrom(w => w.ID))
+                    .ForPath(x => x.UserProfile.Pseudo, q => q.MapFrom(w => w.Pseudo))
+                    .ForPath(x => x.UserProfile.UpuDegree.Name, q => q.MapFrom(w => w.UpuDegree.Name))
+                    .ForPath(x => x.UserProfile.UpuDegree.ID, q => q.MapFrom(w => w.UpuDegree.ID))
+                    .ForPath(x => x.UserProfile.FacebookLink, q => q.MapFrom(w => w.FacebookLink))
+                    .ForPath(x => x.UserProfile.TwitterLink, q => q.MapFrom(w => w.TwitterLink))
+                    .ForPath(x => x.UserProfile.InstagramLink, q => q.MapFrom(w => w.InstagramLink));
         }
     }
 }
