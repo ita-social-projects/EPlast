5d96707b9d915938d54fa91226f72ab87d35158f
Vinnytile <ivan.vinnytskiy@gmail.com>
2021-06-12 10:18:37 +0300

    Changed some AccessGetters

--- EPlast/EPlast.BLL/Services/City/CityAccess/CityAccessGetters/CityAccessForCityAdminGetter.cs
+++ EPlast/EPlast.BLL/Services/City/CityAccess/CityAccessGetters/CityAccessForCityAdminGetter.cs
@@ -14,18 +14,21 @@ namespace EPlast.BLL.Services.City.CityAccess.CityAccessGetters
     {
         private readonly IRepositoryWrapper _repositoryWrapper;
         private readonly AdminType _cityAdminType;
+        private readonly AdminType _cityAdminDeputyType;
 
         public CityAccessForCityAdminGetter(IRepositoryWrapper repositoryWrapper)
         {
             _repositoryWrapper = repositoryWrapper;
             _cityAdminType = _repositoryWrapper.AdminType.GetFirstAsync(
                     predicate: a => a.AdminTypeName == Roles.CityHead).Result;
+            _cityAdminDeputyType = _repositoryWrapper.AdminType.GetFirstAsync(
+                    predicate: a => a.AdminTypeName == Roles.CityHeadDeputy).Result;
         }
 
         public async Task<IEnumerable<DatabaseEntities.City>> GetCities(string userId)
         {
             var cityAdministration = await _repositoryWrapper.CityAdministration.GetFirstOrDefaultAsync(
-                    predicate: c => c.UserId == userId && (DateTime.Now < c.EndDate || c.EndDate == null) && c.AdminTypeId == _cityAdminType.ID);
+                    predicate: c => c.UserId == userId && (DateTime.Now < c.EndDate || c.EndDate == null) && (c.AdminTypeId == _cityAdminType.ID || c.AdminTypeId == _cityAdminDeputyType.ID));
             return cityAdministration != null ? await _repositoryWrapper.City.GetAllAsync(
                 predicate: c => c.ID == cityAdministration.CityId, include: source => source.Include(c => c.Region))
                 : Enumerable.Empty<DatabaseEntities.City>();
@@ -34,7 +37,7 @@ namespace EPlast.BLL.Services.City.CityAccess.CityAccessGetters
         public async Task<IEnumerable<Tuple<int, string>>> GetCitiesIdAndName(string userId)
         {
             var cityAdministration = await _repositoryWrapper.CityAdministration.GetFirstOrDefaultAsync(
-                predicate: c => c.UserId == userId && (DateTime.Now < c.EndDate || c.EndDate == null) && c.AdminTypeId == _cityAdminType.ID);
+                predicate: c => c.UserId == userId && (DateTime.Now < c.EndDate || c.EndDate == null) && (c.AdminTypeId == _cityAdminType.ID || c.AdminTypeId == _cityAdminDeputyType.ID));
             return cityAdministration != null ? (await _repositoryWrapper.City.GetAllAsync(
                     predicate: c => c.ID == cityAdministration.CityId)).Select(c => new Tuple<int, string>(c.ID, c.Name)).ToList()
                 : Enumerable.Empty<Tuple<int, string>>();
