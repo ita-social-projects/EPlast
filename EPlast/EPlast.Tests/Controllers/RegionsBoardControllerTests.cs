d0fca031ac6b803ee081872d00edbf9628625ea5
OleksandrBilan <79528077+OleksandrBilan@users.noreply.github.com>
2021-07-27 19:05:00 +0300

    Add test

--- EPlast/EPlast.Tests/Controllers/RegionsBoardControllerTests.cs
+++ EPlast/EPlast.Tests/Controllers/RegionsBoardControllerTests.cs
@@ -5,28 +5,34 @@ using Moq;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using EPlast.BLL.DTO.Region;
+using EPlast.BLL.Interfaces.Region;
 
 namespace EPlast.Tests.Controllers
 {
     internal class RegionsBoardControllerTests
     {
         private Mock<IRegionsBoardService> _regionsBoardService;
+        private Mock<IRegionService> _regionService;
         private RegionsBoardController _controller;
 
         [SetUp]
         public void SetUp()
         {
             _regionsBoardService = new Mock<IRegionsBoardService>();
-            _controller = new RegionsBoardController(_regionsBoardService.Object);
+            _regionService = new Mock<IRegionService>();
+            _controller = new RegionsBoardController(_regionsBoardService.Object, _regionService.Object);
         }
 
         [Test]
-        public async Task getOrganizations_ReturnsOrganizationsList()
+        public async Task GetUserAccesses_ReturnsAccessList()
         {
             //Arrange
             Dictionary<string, bool> dict = new Dictionary<string, bool>();
             dict.Add("action", It.IsAny<bool>());
-            _regionsBoardService.Setup(x => x.GetUserAccessAsync(It.IsAny<string>())).ReturnsAsync(dict);
+            _regionsBoardService
+                .Setup(x => x.GetUserAccessAsync(It.IsAny<string>()))
+                .ReturnsAsync(dict);
 
             //Act
             var result = await _controller.GetUserAccess(It.IsAny<string>());
@@ -36,6 +42,25 @@ namespace EPlast.Tests.Controllers
             Assert.IsInstanceOf<OkObjectResult>(result);
             Assert.IsNotEmpty(resultValue as Dictionary<string, bool>);
             Assert.IsInstanceOf<Dictionary<string, bool>>(resultValue);
+        }
+
+        [Test]
+        public async Task GetDocuments_ReturnsDocumentsList()
+        {
+            //Arrange
+            List<RegionDocumentDTO> testDocuments = new List<RegionDocumentDTO>() { new RegionDocumentDTO() };
+            _regionService
+                .Setup(x => x.GetRegionDocsAsync(It.IsAny<int>()))
+                .ReturnsAsync(testDocuments);
+
+            //Act
+            var result = await _controller.GetRegionDocs(1);
+            var resultValue = (result as ObjectResult)?.Value;
+
+            //Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            Assert.IsInstanceOf<List<RegionDocumentDTO>>(resultValue);
+            Assert.AreEqual(testDocuments, resultValue as List<RegionDocumentDTO>);
         }
     }
 }
