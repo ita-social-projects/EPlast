9db51a67cb3ab5723cdd1c7d2c47715a9a6b6a9a
Andrii52 <andrii723@gmail.com>
2021-09-07 11:22:41 +0300

    Added tests for new service and controller

---
+++ EPlast/EPlast.Tests/Services/UserAccess/UserAccessServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using EPlast.BLL.Interfaces;
+using EPlast.BLL.Interfaces.Club;
+using EPlast.BLL.Services.UserAccess;
+using EPlast.DataAccess.Entities;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPlast.Tests.Services.UserAccess
+{
+    [TestFixture]
+    class UserAccessServiceTests
+    {
+        private Mock<IClubAccessService> _clubAccessService;
+        private Mock<ISecurityModel> _securityModel;
+
+        private UserAccessService _userAccessService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _clubAccessService = new Mock<IClubAccessService>();
+            _securityModel = new Mock<ISecurityModel>();
+
+            _userAccessService = new UserAccessService(_clubAccessService.Object, _securityModel.Object);
+        }
+
+        [Test]
+        public async Task GetUserClubAccesses_ReturnsListOfClubAccesses()
+        {
+            //Arrange
+            Dictionary<string, bool> dict = new Dictionary<string, bool>();
+            dict.Add("action", It.IsAny<bool>());
+            _securityModel.Setup(x => x.GetUserAccessAsync(It.IsAny<string>(), It.IsAny<IEnumerable<string>>())).ReturnsAsync(dict);
+
+        //Act
+        var result = await _userAccessService.GetUserClubAccessAsync(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<User>());
+
+        //Assert
+        Assert.IsNotEmpty(result);
+            Assert.IsInstanceOf<Dictionary<string, bool>>(result);
+        }
+}
+}
