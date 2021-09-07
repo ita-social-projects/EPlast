55629d0b5332ee1463ab9a0d322751f168c693b0
Andrii52 <andrii723@gmail.com>
2021-09-07 11:19:50 +0300

    Created service for getting user accesses

---
+++ EPlast/EPlast.BLL/Interfaces/UserAccess/IUserAccessService.cs
@@ -0,0 +1,13 @@
+﻿using EPlast.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPlast.BLL.Interfaces.UserAccess
+{
+    public interface IUserAccessService
+    {
+        Task<Dictionary<string, bool>> GetUserClubAccessAsync(int clubId,string userId, User user);
+    }
+}
