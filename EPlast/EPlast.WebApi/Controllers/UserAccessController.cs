b7002b3350562eb938cba04add6ea9985722ba69
Andrii52 <andrii723@gmail.com>
2021-09-07 11:22:08 +0300

    Created controler for geting user accesses

---
+++ EPlast/EPlast.WebApi/Controllers/UserAccessController.cs
@@ -0,0 +1,32 @@
+﻿using EPlast.BLL.Interfaces.UserAccess;
+using EPlast.DataAccess.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPlast.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAccessController : ControllerBase
+    {
+        private IUserAccessService _userAccessService;
+        private readonly UserManager<User> _userManager;
+        public UserAccessController(IUserAccessService userAccessService, UserManager<User> userManager)
+        {
+            _userAccessService = userAccessService;
+            _userManager = userManager;
+        }
+        [HttpGet("GetUserClubAccesses/{clubId}/{userId}")]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        public async Task<IActionResult> GetUserClubAccesses(int clubId, string userId)
+        {
+            return Ok(await _userAccessService.GetUserClubAccessAsync(clubId, userId, await _userManager.GetUserAsync(User)));
+        }
+    }
+}
