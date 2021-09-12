4c587e3f295b0f00f427732e07af9e1f56f66d45
petromonka <petro.monka@gmail.com>
2021-09-12 13:58:03 +0300

    Increased MaxLength in EducationDTO and added migration

---
+++ EPlast/EPlast.DataAccess/Migrations/20210912105553_IncreaseMaxLenghtPlaceOfStudy.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlast.DataAccess.Migrations
+{
+    public partial class IncreaseMaxLenghtPlaceOfStudy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PlaceOfStudy",
+                table: "Educations",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PlaceOfStudy",
+                table: "Educations",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
