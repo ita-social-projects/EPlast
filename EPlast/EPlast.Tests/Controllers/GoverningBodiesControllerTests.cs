59ce99a95cc95eb67fd9048fa32ca13face429b7
Dmytro <84271714+DmytroZelinsky@users.noreply.github.com>
2021-09-26 01:32:47 +0300

    Update GoverningBodiesControllerTests.cs

--- EPlast/EPlast.Tests/Controllers/GoverningBodiesControllerTests.cs
+++ EPlast/EPlast.Tests/Controllers/GoverningBodiesControllerTests.cs
@@ -602,7 +602,7 @@ namespace EPlast.Tests.Controllers
             Assert.IsInstanceOf<NotFoundResult>(result);
         }
         [Test]
-        public async Task EditAnnouncement_ModeStateIsValid_ReturnsOk()
+        public async Task EditAnnouncement_ModelStateIsValid_ReturnsOk()
         {
             //Arrange
             _governingBodyAnnouncementService
