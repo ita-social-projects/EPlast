a37d49e5cb3878e8d26c444b53081c9004078b6a
Roman Bodasyuk <romabodasjuki@gmail.com>
2021-10-22 23:14:56 +0300

    add some checks

---
+++ EPlast/EPlast.BLL/DTO/City/СityAdministrationGetDTO.cs
@@ -0,0 +1,19 @@
+﻿using EPlast.BLL.DTO.Admin;
+using System;
+
+
+namespace EPlast.BLL.DTO.City
+{
+    public class CityAdministrationGetDTO
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public CityUserDTO User { get; set; }
+        public int CityId { get; set; }
+        public int AdminTypeId { get; set; }
+        public AdminTypeDTO AdminType { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
