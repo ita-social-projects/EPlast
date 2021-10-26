b27f2bac1028a861bce802ab47fb03cfc459f173
Roman Bodasyuk <romabodasjuki@gmail.com>
2021-10-27 00:53:15 +0300

    add some changes

---
+++ EPlast/EPlast.BLL/DTO/Region/RegionNamesDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPlast.BLL.DTO.Region
+{
+    public class RegionNamesDTO
+    {
+        public int ID { get; set; }
+        public string RegionName { get; set; }
+    }
+}
---
+++ EPlast/EPlast.DataAccess/Entities/Region/RegionNamesObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPlast.DataAccess.Entities
+{
+    public class RegionNamesObject
+    {
+        public int ID { get; set; }
+        public string RegionName { get; set; }
+    }
+}
