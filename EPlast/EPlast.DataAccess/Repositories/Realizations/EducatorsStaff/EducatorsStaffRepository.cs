d7eafa0bd9baea78f7c9d158947f5512ba49bb10
IlliaDR <omen666i@gmail.com>
2021-12-04 18:46:32 +0200

    Added proper data getting for EducatorsStaff table

--- EPlast/EPlast.DataAccess/Repositories/Realizations/EducatorsStaff/EducatorsStaffRepository.cs
+++ EPlast/EPlast.DataAccess/Repositories/Realizations/EducatorsStaff/EducatorsStaffRepository.cs
@@ -1,12 +1,22 @@
-﻿using EPlast.DataAccess.Entities.EducatorsStaff;
+﻿using System.Collections.Generic;
+using EPlast.DataAccess.Entities.EducatorsStaff;
+using Microsoft.EntityFrameworkCore;
 
 namespace EPlast.DataAccess.Repositories
 {
     public class EducatorsStaffRepository: RepositoryBase<EducatorsStaff>, IEducatorsStaffRepository
     {
         public EducatorsStaffRepository(EPlastDBContext dbContext)
-       : base(dbContext)
+            : base(dbContext)
         {
+        }
+
+        public IEnumerable<EducatorsStaffTableObject> GetEducatorsStaff(int kadraType, string searchData, int page,
+            int pageSize)
+        {
+            return EPlastDBContext.Set<EducatorsStaffTableObject>().FromSqlRaw(
+                "dbo.usp_GetKadras @searchData = {0}, @KadraType = {1}, @PageIndex = {2}, @PageSize = {3} ",
+                searchData, kadraType, page, pageSize);
         }
     }
 }
