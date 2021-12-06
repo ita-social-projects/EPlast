e4afee630b602fc5996803fa9612ff5e01ce2153
NatalyMinziuk <miyukiando333@gmail.com>
2021-12-06 16:23:58 +0200

    namespace changed

--- EPlast/EPlast.BLL/Services/EducatorsStaff/EducatorsStaffService.cs
+++ EPlast/EPlast.BLL/Services/EducatorsStaff/EducatorsStaffService.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EPlast.BLL
+namespace EPlast.BLL.Services
 {
     public class EducatorsStaffService : IEducatorsStaffService
     {
