4b2a2c54afc6b0a6b40c297bebd3a037f5d1eac8
Андрій Захарчук <Andrii723@gmail.com>
2021-12-21 16:54:38 +0200

    Covered new functionality with Unit Tests

---
+++ EPlast/EPlast.Tests/Handlers/Club/GetCountUsersPerYearHandlerTest.cs
@@ -0,0 +1,44 @@
+﻿using EPlast.BLL.Handlers.ClubHandlers;
+using EPlast.BLL.Queries.Club;
+using EPlast.DataAccess.Entities;
+using EPlast.DataAccess.Repositories;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EPlast.Tests.Handlers.Club
+{
+    public class GetCountUsersPerYearHandlerTest
+    {
+        private Mock<IRepositoryWrapper> _repoWrapper;
+        private GetCountUsersPerYearHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _repoWrapper = new Mock<IRepositoryWrapper>();
+            _handler = new GetCountUsersPerYearHandler(_repoWrapper.Object);
+        }
+        [Test]
+        public async Task GetCountUsersPerYearHandlerTest_ReturnsDefaultInteger()
+        {
+            // Arrange
+            _repoWrapper
+               .Setup(r => r.ClubMemberHistory.GetAllAsync(It.IsAny<Expression<Func<ClubMemberHistory, bool>>>(), null))
+               .ReturnsAsync(new List<ClubMemberHistory>());
+
+            // Act
+            var responce = await _handler.Handle(It.IsAny<GetCountUsersPerYearQuery>(), It.IsAny<CancellationToken>());
+
+            // Assert
+            Assert.NotNull(responce);
+            Assert.IsInstanceOf<int>(responce);
+            Assert.AreEqual(responce, 0);
+        }
+    }
+}
