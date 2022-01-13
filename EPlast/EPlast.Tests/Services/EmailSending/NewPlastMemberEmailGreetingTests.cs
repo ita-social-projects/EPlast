ca702fbd13c4c1b45ac8e672bb6f46533d226bd7
YuriiDzioba <86368540+YuriiDzioba@users.noreply.github.com>
2022-01-13 13:47:39 +0200

    Added test

--- EPlast/EPlast.Tests/Services/EmailSending/NewPlastMemberEmailGreetingTests.cs
+++ EPlast/EPlast.Tests/Services/EmailSending/NewPlastMemberEmailGreetingTests.cs
@@ -184,6 +184,123 @@ namespace EPlast.Tests.Services.EmailSending
         }
 
         [Test]
+        public async Task NotifyNewPlastMembersAndCityAdminsAsync_Valid_EmptyCityMembers_Test()
+        {
+            //Arrange
+            var users = new List<CityMembers>()
+            {
+                new CityMembers()
+                {
+                    User = new User(),
+                    City = new DataAccess.Entities.City()
+                }
+            };
+
+            var user = new UserDTO
+            {
+                UserProfile = new UserProfileDTO
+                {
+                    Birthday = DateTime.Now
+                },
+
+                CityMembers = new[]
+                {
+                    new CityMembers
+                    {
+                        City = new DataAccess.Entities.City
+                        {
+                            Name = "CityName"
+                        }
+                    }
+                }
+            };
+
+            var cityProfile = new CityProfileDTO
+            {
+                Admins = new List<CityAdministrationDTO>
+                {
+                    new CityAdministrationDTO
+                    {
+                        User = new CityUserDTO()
+                    }
+                },
+                Head = new CityAdministrationDTO
+                {
+                    User = new CityUserDTO()
+                }
+            };
+
+            var roles = new List<string>()
+            {
+                Roles.Supporter,
+                Roles.FormerPlastMember
+            };
+
+            var confirmedUsers = new ConfirmedUser[]
+            {
+                new ConfirmedUser()
+                {
+                    isClubAdmin = true
+                }
+            }.AsQueryable<ConfirmedUser>();
+            _mockRepoWrapper
+                    .Setup(x => x.CityMembers.GetAllAsync(It.IsAny<Expression<Func<CityMembers, bool>>>(),
+                                                   It.IsAny<Func<IQueryable<CityMembers>, IIncludableQueryable<CityMembers, object>>>()))
+                    .ReturnsAsync(users);
+            _mockUserManager
+                .Setup((x) => x.IsInRoleAsync(It.IsAny<User>(),
+                                              It.IsAny<string>()))
+                .ReturnsAsync(false);
+            _mockUserManager
+                .Setup(x => x.GetRolesAsync(It.IsAny<User>()))
+                .ReturnsAsync(roles);
+            _mockRepoWrapper
+                .Setup(r => r.ConfirmedUser.FindByCondition(It.IsAny<Expression<Func<ConfirmedUser, bool>>>()))
+                .Returns(confirmedUsers);
+            _mockUserManager
+                .Setup(u => u.FindByIdAsync(It.IsAny<string>()))
+                .ReturnsAsync(new User());
+            _mockUserManager
+                .Setup(x => x.AddToRoleAsync(It.IsAny<User>(),
+                                             It.IsAny<string>()));
+            _mockUserService.Setup(x => x.GetUserAsync(It.IsAny<string>()))
+                .ReturnsAsync(user);
+            _mockCityService.Setup(x => x.GetCityAdminsAsync(It.IsAny<int>()))
+                .ReturnsAsync(cityProfile);
+            _mockEmailSendingService
+                .Setup(x => x.SendEmailAsync(It.IsAny<string>(),
+                                             It.IsAny<string>(),
+                                             It.IsAny<string>(),
+                                             It.IsAny<string>()))
+                .ReturnsAsync(true);
+            _mockEmailContentService.Setup(x => x.GetGreetingForNewPlastMemberEmailAsync(It.IsAny<string>()))
+                .Returns(new EmailModel());
+            _mockEmailContentService
+                .Setup(x => x.GetCityAdminAboutNewPlastMemberEmail(It.IsAny<string>(), It.IsAny<string>(),
+                    It.IsAny<DateTime>())).Returns(new EmailModel());
+            _mockEmailContentService.Setup(x => x.GetGreetingForNewPlastMemberMessageAsync(It.IsAny<string>(),
+                It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>())).Returns(new UserNotification());
+            _mockCityService.Setup(x => x.GetCityIdByUserIdAsync(It.IsAny<string>())).ReturnsAsync(240);
+            _mockNotificationService.Setup(x => x.GetAllNotificationTypesAsync()).ReturnsAsync(FakeTypeId());
+            _mockEmailContentService.Setup(x => x.GetGreetingForNewPlastMemberMessageAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>(),
+                    It.IsAny<int>()))
+                .Returns(new UserNotification());
+            _mockRepoWrapper.Setup(x => x.UserNotifications.CreateAsync(It.IsAny<UserNotification>()));
+
+            // Act
+            await _newPlastMemberEmailGreetingService.NotifyNewPlastMembersAndCityAdminsAsync();
+
+            // Assert
+            _mockRepoWrapper.Verify();
+            _mockUserManager.Verify();
+            _mockEmailSendingService.Verify();
+        }
+
+
+        [Test]
         public async Task NotifyNewPlastMembersAndCityAdminsAsync_Valid_Empty_Test()
         {
             // Arrange
