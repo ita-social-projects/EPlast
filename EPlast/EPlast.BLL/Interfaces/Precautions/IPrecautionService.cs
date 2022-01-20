4d4836bd344dd382188a3edcb301816dc2b58b91
Dima <didiakivskyy@gmail.com>
2022-01-20 15:39:17 +0200

    deleted comented code

--- EPlast/EPlast.BLL/Interfaces/Precautions/IPrecautionService.cs
+++ EPlast/EPlast.BLL/Interfaces/Precautions/IPrecautionService.cs
@@ -17,15 +17,7 @@ namespace EPlast.BLL
 
         Task ChangePrecautionAsync(PrecautionDTO precautionDTO, User user);
 
-        Task DeletePrecautionAsync(int id, User user);
-
-        /// <summary>
-        /// Returns all searched Precautions
-        /// </summary>
-        /// <param name="searchedData">Search string</param>
-        /// <param name="page">Current page</param>
-        /// <param name="pageSize">Page size</param>
-        /// <returns>Searched Precautions</returns>
-        Task<Tuple<IEnumerable<UserPrecautionsTableObject>, int>> GetUsersPrecautionsForTableAsync(PrecautionTableSettings tableSettings);//IEnumerable<string> sortByOrder , IEnumerable<string> statusSorter, IEnumerable<string> precautionNameSorter, IEnumerable<string> dateSorter, string searchedData, int page, int pageSize);
+        Task DeletePrecautionAsync(int id, User user);        
+        Task<Tuple<IEnumerable<UserPrecautionsTableObject>, int>> GetUsersPrecautionsForTableAsync(PrecautionTableSettings tableSettings);
     }
 }
