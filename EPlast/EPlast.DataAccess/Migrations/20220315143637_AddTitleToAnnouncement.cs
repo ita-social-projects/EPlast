829bf0e66728cf33393fffa9e45ea51452a0d1ae
Omeliukh-Nazar <91065958+Omeliukh-Nazar@users.noreply.github.com>
2022-03-16 10:25:31 +0200

    Added field Title to Announcement

---
+++ EPlast/EPlast.DataAccess/Migrations/20220315143637_AddTitleToAnnouncement.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlast.DataAccess.Migrations
+{
+    public partial class AddTitleToAnnouncement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Titile",
+                table: "SectorAnnouncement",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Titile",
+                table: "GoverningBodyAnnouncement",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Titile",
+                table: "SectorAnnouncement");
+
+            migrationBuilder.DropColumn(
+                name: "Titile",
+                table: "GoverningBodyAnnouncement");
+        }
+    }
+}
