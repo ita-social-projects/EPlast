390e2f0ad6c0cc3233202c91b62fd0e1e5680607
Omeliukh-Nazar <91065958+Omeliukh-Nazar@users.noreply.github.com>
2022-03-24 10:47:09 +0200

    Added SectorAnnouncementDTO

---
+++ EPlast/EPlast.BLL/DTO/GoverningBody/Announcement/SectorAnnouncementUserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPlast.BLL.DTO.GoverningBody.Announcement
+{
+    public class SectorAnnouncementUserDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string UserId { get; set; }
+        public DateTime Date { get; set; }
+        public UserDTO User { get; set; }
+        public IEnumerable<SectorAnnouncementImageDTO> Images { get; set; }
+        public int SectorId { get; set; }
+        public bool ImagesPresent { get; set; }
+    }
+}
---
+++ EPlast/EPlast.BLL/DTO/GoverningBody/Announcement/SectorAnnouncementWithImagesDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPlast.BLL.DTO.GoverningBody.Announcement
+{
+    public class SectorAnnouncementWithImagesDTO
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Title { get; set; }
+        public string UserId { get; set; }
+        public ICollection<string> ImagesBase64 { get; set; }
+        public int SectorId { get; set; }
+    }
+}
