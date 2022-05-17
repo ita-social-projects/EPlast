27cf68439c446ca5c90bd094985097944014707b
Vardo Ternos <tvardero@gmail.com>
2022-05-17 23:37:06 +0300

    Reorganize EPlastDBContext

--- EPlast/EPlast.DataAccess/EPlastDBContext.cs
+++ EPlast/EPlast.DataAccess/EPlastDBContext.cs
@@ -16,52 +16,104 @@ namespace EPlast.DataAccess
 {
     public class EPlastDBContext : IdentityDbContext<User, IdentityRole, string>
     {
-        public EPlastDBContext(DbContextOptions<EPlastDBContext> options) : base(options)
-        {
-        }
+        public EPlastDBContext(DbContextOptions<EPlastDBContext> options) : base(options) { }
 
-        public DbSet<UserProfile> UserProfiles { get; set; }
-        public DbSet<Nationality> Nationalities { get; set; }
-        public DbSet<Religion> Religions { get; set; }
-        public DbSet<Education> Educations { get; set; }
-        public DbSet<Degree> Degrees { get; set; }
-        public DbSet<Gender> Genders { get; set; }
-        public DbSet<UpuDegree> UpuDegrees { get; set; }
-        public DbSet<Work> Works { get; set; }
-        public DbSet<ConfirmedUser> ConfirmedUsers { get; set; }
+        public DbSet<AchievementDocuments> AchievementDocuments { get; set; }
+        public DbSet<AdminType> AdminTypes { get; set; }
+        public DbSet<AnnualReport> AnnualReports { get; set; }
+        public DbSet<AnnualReportTableObject> AnnualReportTableObjects { get; set; }
         public DbSet<Approver> Approvers { get; set; }
+        public DbSet<BlankBiographyDocuments> BlankBiographyDocuments { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<CityAdministration> CityAdministrations { get; set; }
+        public DbSet<CityDocuments> CityDocuments { get; set; }
+        public DbSet<CityDocumentType> CityDocumentTypes { get; set; }
+        public DbSet<CityLegalStatus> CityLegalStatuses { get; set; }
+        public DbSet<CityMembers> CityMembers { get; set; }
+        public DbSet<CityObject> CityObjects { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<ClubAdministration> ClubAdministrations { get; set; }
+        public DbSet<ClubAnnualReport> ClubAnnualReports { get; set; }
+        public DbSet<ClubAnnualReportTableObject> ClubAnnualReportTableObjects { get; set; }
+        public DbSet<ClubDocuments> ClubDocuments { get; set; }
+        public DbSet<ClubDocumentType> ClubDocumentTypes { get; set; }
+        public DbSet<ClubLegalStatus> ClubLegalStatuses { get; set; }
+        public DbSet<ClubMemberHistory> ClubMemberHistory { get; set; }
+        public DbSet<ClubMembers> ClubMembers { get; set; }
+        public DbSet<ClubReportAdmins> ClubReportAdmins { get; set; }
+        public DbSet<ClubReportCities> ClubReportCities { get; set; }
+        public DbSet<ClubReportMember> ClubReportMember { get; set; }
+        public DbSet<ClubReportPlastDegrees> ClubReportPlastDegrees { get; set; }
+        public DbSet<ConfirmedUser> ConfirmedUsers { get; set; }
+        public DbSet<Decesion> Decesions { get; set; }
+        public DbSet<DecesionTarget> DecesionTargets { get; set; }
+        public DbSet<DecisionTableObject> DecisionTableObject { get; set; }
+        public DbSet<Degree> Degrees { get; set; }
+        public DbSet<Distinction> Distinctions { get; set; }
+        public DbSet<DocumentTemplate> DocumentTemplates { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<EducatorsStaff> KVs { get; set; }
+        public DbSet<EducatorsStaffTableObject> EducatorsStaffTableObjects { get; set; }
+        public DbSet<EducatorsStaffTypes> KVTypes { get; set; }
         public DbSet<Event> Events { get; set; }
-        public DbSet<Gallary> Gallarys { get; set; }
-        public DbSet<EventGallary> EventGallarys { get; set; }
-        public DbSet<ParticipantStatus> ParticipantStatuses { get; set; }
-        public DbSet<Participant> Participants { get; set; }
-        public DbSet<EventCategory> EventCategories { get; set; }
-        public DbSet<EventType> EventTypes { get; set; }
-        public DbSet<EventCategoryType> EventCategoryTypes { get; set; }
-        public DbSet<EventStatus> EventStatuses { get; set; }
         public DbSet<EventAdministration> EventAdministration { get; set; }
         public DbSet<EventAdministrationType> EventAdministrationType { get; set; }
-        public DbSet<UserTableObject> UserTableObjects { get; set; }
-        public DbSet<RegionObject> RegionObjects { get; set; }
-        public DbSet<CityObject> CityObjects { get; set; }
-        public DbSet<RegionNamesObject> RegionNamesObjects { get; set; }
-        public DbSet<Section> Sections { get; set; }
-        public DbSet<Subsection> Subsections { get; set; }
-        public DbSet<SubsectionPictures> SubsectionsPictures { get; set; }
-        public DbSet<Pictures> Pictures { get; set; }
-        public DbSet<AnnualReportTableObject> AnnualReportTableObjects { get; set; }
-        public DbSet<ClubAnnualReportTableObject> ClubAnnualReportTableObjects { get; set; }
-        public DbSet<RegionAnnualReportTableObject> RegionAnnualReportTableObjects { get; set; }
-        public DbSet<UserDistinctionsTableObject> UserDistinctionsTableObject { get; set; }
-        public DbSet<MethodicDocumentTableObject> MethodicDocumentTableObjects { get; set; }
-        public DbSet<DecisionTableObject> DecisionTableObject { get; set; }
-        public DbSet<RegionMembersInfoTableObject> RegionMembersInfoTableObjects { get; set; }
+        public DbSet<EventCategory> EventCategories { get; set; }
+        public DbSet<EventCategoryType> EventCategoryTypes { get; set; }
+        public DbSet<EventGallary> EventGallarys { get; set; }
+        public DbSet<EventSection> EventSection { get; set; }
+        public DbSet<EventStatus> EventStatuses { get; set; }
+        public DbSet<EventType> EventTypes { get; set; }
+        public DbSet<ExtractFromUPUDocuments> ExtractFromUPUDocuments { get; set; }
+        public DbSet<Gallary> Gallarys { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<GoverningBodyAdministration> GoverningBodyAdministrations { get; set; }
         public DbSet<GoverningBodyAnnouncement> GoverningBodyAnnouncement { get; set; }
         public DbSet<GoverningBodyAnnouncementImage> GoverningBodyAnnouncementImages { get; set; }
+        public DbSet<GoverningBodyDocuments> GoverningBodyDocuments { get; set; }
+        public DbSet<GoverningBodyDocumentType> GoverningBodyDocumentTypes { get; set; }
+        public DbSet<MembersStatistic> MembersStatistics { get; set; }
+        public DbSet<MethodicDocument> MethodicDocuments { get; set; }
+        public DbSet<MethodicDocumentTableObject> MethodicDocumentTableObjects { get; set; }
+        public DbSet<Nationality> Nationalities { get; set; }
+        public DbSet<NotificationType> NotificationTypes { get; set; }
+        public DbSet<Organization> Organization { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<ParticipantStatus> ParticipantStatuses { get; set; }
+        public DbSet<Pictures> Pictures { get; set; }
+        public DbSet<PlastDegree> PlastDegrees { get; set; }
+        public DbSet<Precaution> Precautions { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<RegionAdministration> RegionAdministrations { get; set; }
+        public DbSet<RegionAnnualReport> RegionAnnualReports { get; set; }
+        public DbSet<RegionAnnualReportTableObject> RegionAnnualReportTableObjects { get; set; }
+        public DbSet<RegionDocuments> RegionDocs { get; set; }
+        public DbSet<RegionFollowers> RegionFollowers { get; set; }
+        public DbSet<RegionMembersInfoTableObject> RegionMembersInfoTableObjects { get; set; }
+        public DbSet<RegionNamesObject> RegionNamesObjects { get; set; }
+        public DbSet<RegionObject> RegionObjects { get; set; }
+        public DbSet<Religion> Religions { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Sector> GoverningBodySectors { get; set; }
+        public DbSet<SectorAdministration> GoverningBodySectorAdministrations { get; set; }
+        public DbSet<SectorDocuments> GoverningBodySectorDocuments { get; set; }
+        public DbSet<SectorDocumentType> GoverningBodySectorDocumentTypes { get; set; }
+        public DbSet<Subsection> Subsections { get; set; }
+        public DbSet<SubsectionPictures> SubsectionsPictures { get; set; }
         public DbSet<Terms> Terms { get; set; }
+        public DbSet<UpuDegree> UpuDegrees { get; set; }
+        public DbSet<UserDistinction> UserDistinctions { get; set; }
+        public DbSet<UserDistinctionsTableObject> UserDistinctionsTableObject { get; set; }
+        public DbSet<UserMembershipDates> UserMembershipDates { get; set; }
+        public DbSet<UserNotification> UserNotifications { get; set; }
+        public DbSet<UserPlastDegree> UserPlastDegrees { get; set; }
+        public DbSet<UserPrecaution> UserPrecautions { get; set; }
+        public DbSet<UserPrecautionsTableObject> UserPrecautionsTableObject { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
         public DbSet<UserRenewal> UserRenewals { get; set; }
         public DbSet<UserRenewalsTableObject> UserRenewalsTableObjects { get; set; }
-        public DbSet<EducatorsStaffTableObject> EducatorsStaffTableObjects { get; set; }
+        public DbSet<UserTableObject> UserTableObjects { get; set; }
+        public DbSet<Work> Works { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -271,65 +323,5 @@ namespace EPlast.DataAccess
                 annualReport.HasOne(a => a.Club);
             });
         }
-        public DbSet<RegionAnnualReport> RegionAnnualReports { get; set; }
-        public DbSet<DocumentTemplate> DocumentTemplates { get; set; }
-        public DbSet<DecesionTarget> DecesionTargets { get; set; }
-        public DbSet<Decesion> Decesions { get; set; }
-        public DbSet<MethodicDocument> MethodicDocuments { get; set; }
-        public DbSet<AnnualReport> AnnualReports { get; set; }
-        public DbSet<ClubAnnualReport> ClubAnnualReports { get; set; }
-
-        public DbSet<MembersStatistic> MembersStatistics { get; set; }
-
-        public DbSet<Organization> Organization { get; set; }
-        public DbSet<GoverningBodyAdministration> GoverningBodyAdministrations { get; set; }
-        public DbSet<GoverningBodyDocuments> GoverningBodyDocuments { get; set; }
-        public DbSet<GoverningBodyDocumentType> GoverningBodyDocumentTypes { get; set; }
-
-        public DbSet<Sector> GoverningBodySectors { get; set; }
-        public DbSet<SectorAdministration> GoverningBodySectorAdministrations { get; set; }
-        public DbSet<SectorDocuments> GoverningBodySectorDocuments { get; set; }
-        public DbSet<SectorDocumentType> GoverningBodySectorDocumentTypes { get; set; }
-
-        public DbSet<City> Cities { get; set; }
-        public DbSet<CityAdministration> CityAdministrations { get; set; }
-        public DbSet<CityDocuments> CityDocuments { get; set; }
-        public DbSet<CityDocumentType> CityDocumentTypes { get; set; }
-        public DbSet<CityMembers> CityMembers { get; set; }
-
-        public DbSet<AdminType> AdminTypes { get; set; }
-
-        public DbSet<Club> Clubs { get; set; }
-        public DbSet<ClubAdministration> ClubAdministrations { get; set; }
-        public DbSet<ClubDocuments> ClubDocuments { get; set; }
-        public DbSet<ClubDocumentType> ClubDocumentTypes { get; set; }
-        public DbSet<ClubMembers> ClubMembers { get; set; }
-
-        public DbSet<Region> Regions { get; set; }
-        public DbSet<RegionDocuments> RegionDocs { get; set; }
-        public DbSet<RegionAdministration> RegionAdministrations { get; set; }
-        public DbSet<RegionFollowers> RegionFollowers { get; set; }
-        public DbSet<CityLegalStatus> CityLegalStatuses { get; set; }
-        public DbSet<ClubLegalStatus> ClubLegalStatuses { get; set; }
-        public DbSet<UserPlastDegree> UserPlastDegrees { get; set; }
-        public DbSet<UserMembershipDates> UserMembershipDates { get; set; }
-        public DbSet<PlastDegree> PlastDegrees { get; set; }
-        public DbSet<EducatorsStaff> KVs { get; set; }
-        public DbSet<EducatorsStaffTypes> KVTypes { get; set; }
-        public DbSet<Precaution> Precautions { get; set; }
-        public DbSet<Distinction> Distinctions { get; set; }
-        public DbSet<UserPrecaution> UserPrecautions { get; set; }
-        public DbSet<UserDistinction> UserDistinctions { get; set; }
-        public DbSet<BlankBiographyDocuments> BlankBiographyDocuments { get; set; }
-        public DbSet<NotificationType> NotificationTypes { get; set; }
-        public DbSet<UserNotification> UserNotifications { get; set; }
-        public DbSet<AchievementDocuments> AchievementDocuments { get; set; }
-        public DbSet<ExtractFromUPUDocuments> ExtractFromUPUDocuments { get; set; }
-        public DbSet<EventSection> EventSection { get; set; }
-        public DbSet<ClubReportPlastDegrees> ClubReportPlastDegrees { get; set; }
-        public DbSet<ClubReportMember> ClubReportMember { get; set; }
-        public DbSet<ClubReportAdmins> ClubReportAdmins { get; set; }
-        public DbSet<ClubMemberHistory> ClubMemberHistory { get; set; }
-        public DbSet<ClubReportCities> ClubReportCities { get; set; }
     }
 }
