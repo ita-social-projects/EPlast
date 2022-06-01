1ece8e9fceb6c7de7279e71a99b9692f453d31c6
Tomash-Klaus <tomashivskymykola@gmail.com>
2022-06-01 19:57:52 +0300

    add new property for participant model
    property wasPresent

---
+++ EPlast/EPlast.DataAccess/Migrations/20220601164936_wasPresent_property_for_participant.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlast.DataAccess.Migrations
+{
+    public partial class wasPresent_property_for_participant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
