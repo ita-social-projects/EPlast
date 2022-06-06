7647c0de62e86576b1b69383c3d50cf38f0e866a
Omeliukh-Nazar <91065958+Omeliukh-Nazar@users.noreply.github.com>
2022-06-06 09:30:55 +0300

    Added migrations to update GoverningBodyAdministration entity

---
+++ EPlast/EPlast.DataAccess/Migrations/20220606062837_addedGoverningBodyAdminRoleToGoverningBodyAdminstrationEntity.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlast.DataAccess.Migrations
+{
+    public partial class addedGoverningBodyAdminRoleToGoverningBodyAdminstrationEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SectorAnnouncementImage");
+
+            migrationBuilder.DropTable(
+                name: "SectorAnnouncement");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GoverningBodyAdminRole",
+                table: "GoverningBodyAdministrations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GoverningBodyAdminRole",
+                table: "GoverningBodyAdministrations");
+
+            migrationBuilder.CreateTable(
+                name: "SectorAnnouncement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SectorId = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SectorAnnouncement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SectorAnnouncement_GoverningBodySectors_SectorId",
+                        column: x => x.SectorId,
+                        principalTable: "GoverningBodySectors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SectorAnnouncement_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SectorAnnouncementImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SectorAnnouncementId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SectorAnnouncementImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SectorAnnouncementImage_SectorAnnouncement_SectorAnnouncementId",
+                        column: x => x.SectorAnnouncementId,
+                        principalTable: "SectorAnnouncement",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectorAnnouncement_SectorId",
+                table: "SectorAnnouncement",
+                column: "SectorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectorAnnouncement_UserId",
+                table: "SectorAnnouncement",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectorAnnouncementImage_SectorAnnouncementId",
+                table: "SectorAnnouncementImage",
+                column: "SectorAnnouncementId");
+        }
+    }
+}
