39d3a37e4033fe9ed14f0c9950d6f3fc7e154b27
Hryhorii Voloshyn <54772120+hryhoriivoloshyn@users.noreply.github.com>
2022-06-06 17:36:28 +0300

    Dropped SectorAnnouncementImage, SectorAnnouncement, UserPrecautionsT… (#2544)
    * Dropped SectorAnnouncementImage, SectorAnnouncement, UserPrecautionsTableObject
    
    * Removed comments
    
    * I changed [EPlastDBContextModelSnapshot] so it would not show SectorAnnouncementImage, SectorAnnouncement, UserPrecautionsTableObject tables.

---
+++ EPlast/EPlast.DataAccess/Migrations/20220606135303_DropTableUserPrecautionsTableObjectAndSectorAnnouncement.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlast.DataAccess.Migrations
+{
+    public partial class DropTableUserPrecautionsTableObjectAndSectorAnnouncement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SectorAnnouncementImage");
+
+            migrationBuilder.DropTable(
+                name: "SectorAnnouncement");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SectorAnnouncement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SectorId = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SectorAnnouncement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SectorAnnouncement_GoverningBodySectors_SectorId",
+                        column: x => x.SectorId,
+                        principalTable: "GoverningBodySectors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SectorAnnouncement_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPrecautionsTableObject",
+                columns: table => new
+                {
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    PrecautionName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Reporter = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Total = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SectorAnnouncementImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SectorAnnouncementId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SectorAnnouncementImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SectorAnnouncementImage_SectorAnnouncement_SectorAnnouncementId",
+                        column: x => x.SectorAnnouncementId,
+                        principalTable: "SectorAnnouncement",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectorAnnouncement_SectorId",
+                table: "SectorAnnouncement",
+                column: "SectorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectorAnnouncement_UserId",
+                table: "SectorAnnouncement",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectorAnnouncementImage_SectorAnnouncementId",
+                table: "SectorAnnouncementImage",
+                column: "SectorAnnouncementId");
+        }
+    }
+}
