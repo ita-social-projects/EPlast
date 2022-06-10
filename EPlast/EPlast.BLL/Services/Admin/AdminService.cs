7cc8e312926da67b72c1390347934b5434d7f4e7
Hryhorii-V <hryhorii.voloshyn@nure.ua>
2022-06-10 16:52:34 +0300

    Removed unnecessary variables

--- EPlast/EPlast.BLL/Services/Admin/AdminService.cs
+++ EPlast/EPlast.BLL/Services/Admin/AdminService.cs
@@ -348,15 +348,13 @@ namespace EPlast.BLL.Services
             foreach (var user in users)
             {
                 var roles = await _userManager.GetRolesAsync(user);
-                    if (roles.Contains(Roles.PlastMember))
-                    {
-                        var IsInDeputyRole = roles.Intersect(adminRoles).Any();
-                        var isInLowerRole = roles.Intersect(Roles.LowerRoles).Any();
-                        var shortUser = _mapper.Map<User, ShortUserInformationDTO>(user);
-                        shortUser.IsInDeputyRole = IsInDeputyRole;
-                        shortUser.IsInLowerRole = isInLowerRole;
-                        usersDtos.Add(shortUser);
-                    }
+                if (roles.Contains(Roles.PlastMember))
+                {
+                    var shortUser = _mapper.Map<User, ShortUserInformationDTO>(user);
+                    shortUser.IsInDeputyRole = roles.Intersect(adminRoles).Any();
+                    shortUser.IsInLowerRole = roles.Intersect(Roles.LowerRoles).Any();
+                    usersDtos.Add(shortUser);
+                }
             }
             return usersDtos;
         }
