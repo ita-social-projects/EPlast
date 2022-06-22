8a0a8b9d672f3f87b68c12717f245ad48ab657b5
v4k <55637550+vsynieok@users.noreply.github.com>
2022-06-23 00:11:57 +0300

    Add validation for Club model

--- EPlast/EPlast.WebApi/Models/Club/ClubViewModel.cs
+++ EPlast/EPlast.WebApi/Models/Club/ClubViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EPlast.WebApi.Models.Club
 {
     public class ClubViewModel
     {
         public int ID { get; set; }
+        [Required, MaxLength(200, ErrorMessage = "Назва куреня не може бути довшою за 200 символів.")]
         public string Name { get; set; }
+        [MaxLength(18, ErrorMessage = "Контактний телефон куреня не може бути довшим за 12 символів.")]
         public string PhoneNumber { get; set; }
+        [Required, MaxLength(50, ErrorMessage = "Електронна адрес куреня не може бути довшою за 50 символів.")]
         public string Email { get; set; }
+        [MaxLength(256, ErrorMessage = "Посилання куреня не може бути довшим за 256 символів.")]
         public string ClubURL { get; set; }
+        [MaxLength(1000, ErrorMessage = "Опис куреня не може бути довшим за 1000 символів.")]
         public string Description { get; set; }
         public string Slogan { get; set; }
         public string Logo { get; set; }
