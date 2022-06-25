b63db2985561c39fe54f60a3ec8f36752e83dfcb
Hryhorii-V <hryhorii.voloshyn@nure.ua>
2022-06-25 15:24:38 +0300

    Added neccessary seeding data to PlastDegree entity, Removed AnnualReport decimal type warnings.

--- EPlast/EPlast.DataAccess/Migrations/20220625122124_SeedingPlastDegreesFix.Designer.cs
+++ EPlast/EPlast.DataAccess/Migrations/20220625122124_SeedingPlastDegreesFix.Designer.cs
@@ -4,14 +4,16 @@ using EPlast.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EPlast.DataAccess.Migrations
 {
     [DbContext(typeof(EPlastDBContext))]
-    partial class EPlastDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220625122124_SeedingPlastDegreesFix")]
+    partial class SeedingPlastDegreesFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
