f3a71f8eccd8a633d4534a08e06d18aaff1ed7fb
Vladyslav Synieok <55637550+vsynieok@users.noreply.github.com>
2022-07-01 10:12:11 +0300

    Add Oblast field to RegionFollower model

---
+++ EPlast/EPlast.DataAccess/Migrations/20220701065839_AddOblastToRegionFollower.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlast.DataAccess.Migrations
+{
+    public partial class AddOblastToRegionFollower : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Oblast",
+                table: "UserTableObjects",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Oblast",
+                table: "RegionFollowers",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Oblast",
+                table: "UserTableObjects");
+
+            migrationBuilder.DropColumn(
+                name: "Oblast",
+                table: "RegionFollowers");
+        }
+    }
+}
