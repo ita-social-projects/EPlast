c74841afe2740e52a3a6032c0524d29e9d93fb7b
Vladyslav Synieok <55637550+vsynieok@users.noreply.github.com>
2022-07-01 14:36:37 +0300

    Fix Cyrillic variable name

---
+++ EPlast/EPlast.DataAccess/Migrations/20220701112456_fixCyrillicName.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlast.DataAccess.Migrations
+{
+    public partial class fixCyrillicName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "СityURL",
+                table: "RegionFollowers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CityURL",
+                table: "RegionFollowers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CityURL",
+                table: "RegionFollowers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "СityURL",
+                table: "RegionFollowers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
