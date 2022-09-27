4fcba039f42d68f8053780085675d45eed161103
Hryhorii-V <grigoriyvoloshyn@gmail.com>
2022-09-27 10:13:21 +0300

    Revert unsecure debug merge
    This commit reverts: 150b4eebc0db95ebff662217cc35b5670c7ef71a

--- EPlast/EPlast.WebApi/Controllers/AuthController.cs
+++ EPlast/EPlast.WebApi/Controllers/AuthController.cs
@@ -1,8 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using AutoMapper;
 using EPlast.BLL.DTO.Account;
 using EPlast.BLL.Interfaces;
@@ -15,7 +13,6 @@ using EPlast.Resources;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using MimeKit;
 
 namespace EPlast.WebApi.Controllers
@@ -259,7 +256,7 @@ namespace EPlast.WebApi.Controllers
         {
             var reciever = new MailboxAddress($"{user.FirstName} {user.LastName}", user.Email);
 
-            var token = HttpUtility.UrlEncode(await _userManager.GenerateEmailConfirmationTokenAsync(user));
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
             string url = Url.Action(
                 "ConfirmEmail",
